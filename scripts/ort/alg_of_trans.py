ae082fcca1c1e595df2dd0ba526cda96da9cbd5d
Daniel Bliss <dbliss@berkeley.edu>
2011-04-03 18:29:41 -0700

    Finish first pass at full ORT implementation.
    Still needs to be tested and fully debugged.

--- scripts/ort/alg_of_trans.py
+++ scripts/ort/alg_of_trans.py
@@ -21,7 +21,7 @@ import networkx as nx
 
 dir = '/home/despo/dbliss/cocomac/'
 
-with open('%sgraphs/mapping/deduced_additional_edges_mapping_graph.pck' %
+with open('%sgraphs/mapping/deduced_additional_edges_modha_mapping_graph.pck' %
           dir) as f:
     MAPPING_GRAPH = pickle.load(f)
 
@@ -29,6 +29,13 @@ AREAS = copy.deepcopy(MAPPING_GRAPH.nodes())
 
 with open('%smaps.pck' % dir) as f:
     MAPS = pickle.load(f)
+
+with open('%smodha_merged_connectivity_graph.pck' % dir) as f:
+    CONNECTIVITY_GRAPH = pickle.load(f)
+
+EDGES = CONNECTIVITY_GRAPH.edges()
+
+TARGET_MAP = 'PHT00'
 
 #-----------------------------------------------------------------------------
 # Class Definitions
@@ -147,7 +154,7 @@ def transformation_step(EC_prev_B_k, RC_A_i_B_k, EC_A_i):
                             #              'X': 'U',
                             #              'C': 'X'
                             #              }
-                                  },
+                            #      },
                             'P': {'S': {'N': 'P',
                                         'P': 'P',
                                         'X': 'P',
@@ -183,27 +190,104 @@ def transformation_step(EC_prev_B_k, RC_A_i_B_k, EC_A_i):
                                   }
                             }
     return transformation_rules[EC_prev_B_k][RC_A_i_B_k][EC_A_i]
+
+def get_EC(source, target, which):
+    """Get a region's EC for a specific connection.
+
+    Parameters
+    ----------
+    source : string
+      The source region's full CoCoMac name (e.g., PP99-47/12).
+
+    target : string
+      The target region's full CoCoMac name.
+
+    which : string
+      Specification of the region for which the EC is desired.
+
+    Returns
+    -------
+    EC : string
+      Region of interest's EC for the specified connection.
+    """
+    if g.has_edge(source, target):
+        if g.edge[source][target]['Density']['Degree'] != 0:
+            if which == 'pred':
+                return g.edge[source][target]['EC_s']
+            elif which == 'succ':
+                return g.edge[source][target]['EC_t']
+            else:
+                raise TypeError, 'which must be "pred" or "succ."'
+        else:
+            return 'N'
+    else:
+        return 'U'
+
+def ort(area, target_map, pred=None, succ=None):
+    """Get sets phi_B and phi_A.
+
+    Parameters
+    ----------
+    area : string
+      Area with BrainMap prefix in CoCoMac format (e.g., PP94-46).
+
+    target_map : string
+      BrainMap in CoCoMac format (e.g., PHT00).
+
+    Returns
+    -------
+    phi_A : dict
+      phi_A is a dict with a key for every element of phi_B, and values
+      corresponding to regions in the input area's map that are coextensive
+      with these B regions.
+    """
+    phi_B = find_coextensive_areas(area, target_map)
+    for B_k in phi_B:
+        phi_A = find_coextensive_areas(B_k, area.split('-')[0])
+        EC_B_k = 'B'
+        for A_i in phi_A:
+            RC_A_i_B_k = MAPPING_GRAPH.edge[A_i][B_k]['RC_final']
+            if succ:
+                EC_A_i = get_EC(A_i, succ, 'pred')
+            elif pred:
+                EC_A_i = get_EC(pred, A_i, 'succ')
+            else:
+                raise TypeError, 'Must provide pred or succ as parameter.'
+            EC_B_k = transformation_step(EC_B_k, RC_A_i_B_k, EC_A_i)
+        EC_res[B_k] = EC_B_k
+    return EC_res
     
 #-----------------------------------------------------------------------------
 # Main script
 #-----------------------------------------------------------------------------
 
-for A_prime_label in MAPS - set(['PHT00']):
-    A_prime = set()
-    for area in AREAS:
-        if A_prime_label in area:
-            A_prime.add(area)
-    for A in A_prime:
-        AREAS.remove(A)
-        B = find_coextensive_areas(A, 'PHT00')
-        for B_k in B:
-            A_others = find_coextensive_areas(B_k, A_prime_label) - set([A])
-            EC_B_k = 'B'
-            for A_i in A_others:
-                RC_A_i_B_k = MAPPING_GRAPH.edge[A_i][B_k]['RC_final']
-                #Big question is where to get EC_A_i. Probably from
-                #connectivity data.
-                EC_B_k = transformation_step(EC_B_k, RC_A_i_B_k, EC_A_i)
+transformed_g = nx.DiGraph()
+
+for edge in EDGES:
+    pred = edge[0]
+    succ = edge[1]
+    ec_pred = ort(pred, TARGET_MAP, succ=succ)
+    ec_succ = ort(succ, TARGET_MAP, pred=pred)
+    for new_pred, EC_s in ec_pred.iteritems():
+        for new_succ, EC_t in ec_succ.iteritems():
+            transformed_g.add_edge(new_pred, new_succ, EC_s=EC_s, EC_t=EC_t)
+
+#for A_prime_label in MAPS - set(['PHT00']):
+#    A_prime = set()
+#    for area in AREAS:
+#        if A_prime_label in area:
+#            A_prime.add(area)
+#    for A_alpha in A_prime:
+#        AREAS.remove(A)
+#        phi_B = find_coextensive_areas(A_alpha, 'PHT00')
+#        for B_k in phi_B:
+#            phi_A = find_coextensive_areas(B_k, A_prime_label)
+#            EC_B_k = 'B'
+#            for A_i in phi_A:
+#                RC_A_i_B_k = MAPPING_GRAPH.edge[A_i][B_k]['RC_final']
+#                #Big question is where to get EC_A_i. Probably from
+#                #connectivity data.
+#                EC_B_k = transformation_step(EC_B_k, RC_A_i_B_k, EC_A_i)
 
 ##############################################################################
 # Test section - eventually will be moved to another file
