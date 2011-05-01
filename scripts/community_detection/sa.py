93ef6ddb2425b4fba761c49d9ff2efcbf33b60cc
Daniel Bliss <dbliss@berkeley.edu>
2011-04-30 17:46:31 -0700

    Run SA on PHT00 whole brain.

--- scripts/community_detection/sa.py
+++ scripts/community_detection/sa.py
@@ -5,37 +5,62 @@
 """
 
 import pickle
-import copy
+import os
 
 import networkx as nx
-import numpy as np
+import nose.tools as nt
 
-from brainx import detect_modules as dm
+from brainx import modularity as m
 
-def sa():
-    """Run SA on a given graph and pickle the results."""
+OUTPUT_FNAME = 'pht00_round2'
 
-    homedir = '/home/despo/dbliss/cocomac/'
+def relabel_part(num_part, num2name):
+    name_part = {}
+    for mod_label, mod_members in num_part.iteritems():
+        name_set = set()
+        for member in mod_members:
+            name_set.add(num2name[member])
+        name_part[mod_label] = name_set
+    return name_part
 
-    # Open graph to be analyzed.
-    f = open('%sgraphs/lateral_graph.pck' % homedir)
-    g = pickle.load(f)
-    f.close()
-    g = g.to_undirected()
-    results = dm.simulated_annealing(g)
+def make_name_num_dicts(name_g):
+    name2num, num2name = {}, {}
+    for i, node in enumerate(name_g.nodes()):
+        name2num[node] = i
+        num2name[i] = node
+    return name2num, num2name
 
-    # Open dictionary mapping region numbers to names.
-    f = open('%snumbering/lat_num2name.pck' % homedir)
-    num = pickle.load(f)
-    f.close()
-    final_part = {}
-    for mod in results.index:
-        final_part[mod] = [num[node] for node in results.index[mod]]
-
-    # Open an appropriately named pickle.
-    p = open('%smod_results/within_pfc/lat_part.pck' % homedir, 'w')
-    pickle.dump(final_part, p)
-    p.close()
+def main(name_g):
+    name2num, num2name = name_num_dicts(name_g)
+    num_g = nx.relabel_nodes(name_g, name2num)
+    num_g = num_g.to_undirected()
+    best_part = m.simulated_annealing(num_g)
+    return relabel_part(best_part.index), best_part.modularity()
 
 if __name__ == '__main__':
-    sa()
+    os.chdir('../ort/results_ort')
+    with open('final_g2.pck') as f:
+        name_g = pickle.load(f)
+
+    part, q = main(name_g)
+
+    os.chdir('../../community_detection/results_sa')
+    with open('%s.pck' % OUTPUT_FNAME, 'w') as f:
+        pickle.dump(part, f)
+        pickle.dump(q, f)
+
+class Tests(object):
+    def __init__(self):
+        self.num2name = {0: 'a', 1: 'c', 2: 'b', 3: 'e', 4: 'd'}
+        self.name_g = nx.DiGraph()
+        self.name_g.add_nodes_from(['a', 'b', 'c', 'd', 'e'])
+
+    def test_make_name_num_dicts(self):
+        name2num = {'a': 0, 'b': 2, 'c': 1, 'd': 4, 'e': 3}
+        nt.assert_equal(make_name_num_dicts(self.name_g)[0], name2num)
+        nt.assert_equal(make_name_num_dicts(self.name_g)[1], self.num2name)
+
+    def test_relabel_part(self):
+        input_part = {0: set([1, 3]), 1: set([0, 4]), 2: set([2])}
+        desired_part = {0: set(['c', 'e']), 1: set(['a', 'd']), 2: set(['b'])}
+        nt.assert_equal(relabel_part(input_part, self.num2name), desired_part)
