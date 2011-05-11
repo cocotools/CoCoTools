7400e92bf8ae670ad9e78c27bd17d8977dd53df5
Daniel Bliss <dbliss@berkeley.edu>
2011-05-10 18:01:40 -0700

    Wait till end of eliminate_contradictions procedure to remove edges.
    Keep track of them in Eliminator.bad_edges along the way. Replace list searches
    with set searches in some cases to speed up the search.

--- ort/eliminate_contradictions.py
+++ ort/eliminate_contradictions.py
@@ -23,6 +23,7 @@ class Eliminator(object):
         self.map_g = map_g
         self.region = ''
         self.blank = ''
+        self.bad_edges = set()
 
     def paths_blank_same_map(self):
         """
@@ -55,8 +56,8 @@ class Eliminator(object):
                     return reg_same_map, rcs
         return False
 
-    def remove_edges(self, others):
-        """Remove edges that depend on relation between others and self.region.
+    def keep_track_of_edges_to_remove(self, others):
+        """Mark edges responsible for a contradiction for removal.
 
         First, we remove the edges between self.region and each other. This,
         however, is not enough. Stephan et al. fail to note that we must also
@@ -82,79 +83,81 @@ class Eliminator(object):
         """
         for other in others:
 
-            self.map_g.remove_edge(self.region, other)
-            self.map_g.remove_edge(other, self.region)
+            self.bad_edges.add((self.region, other))
+            self.bad_edges.add((other, self.region))
 
-            finished = False
+            for source, target in self.map_g.edges_iter():
 
-            while not finished:
+                #Keep track of dependent edges with variable bad.
+                bad = []
 
-                for source, target in self.map_g.edges_iter():
+                #See whether (other, self.region) or (self.region, other)
+                #is in either of source <--> target's tpaths.
+                tpath1 = self.map_g[source][target]['tpath']
+                tpath2 = self.map_g[target][source]['tpath']
 
-                    #Keep track of dependent edges with variable bad.
-                    bad = []
+                if (tpath1 == [self.region, other] or
+                    tpath1 == [other, self.region]):
+                    bad.extend([source, target])
+                elif self.region in set(tpath1):
 
-                    #See whether (other, self.region) or (self.region, other)
-                    #is in either of source <--> target's tpaths.
-                    tpath1 = self.map_g[source][target]['tpath']
-                    tpath2 = self.map_g[target][source]['tpath']
-
-                    if self.region in tpath1:
-
-                        #We need to see whether the region in tpath1 after or
-                        #before self.region is other. But we need to handle the
-                        #possibility that self.region is the first or last
-                        #region in tpath.
-                        try:
-                            if tpath1[tpath1.index(self.region)+1] == other:
-                                bad.extend([source, target])
-                        except IndexError:
-                            pass
-                        try:
-                            if tpath1[tpath1.index(self.region)-1] == other:
-                                bad.extend([source, target])
-                        except IndexError:
-                            pass
-
-                    if self.region in tpath2:
-
-                        try:
-                            if tpath2[tpath2.index(self.region)+1] == other:
-                                bad.extend([target, source])
-                        except IndexError:
-                            pass
-                        try:
-                            if tpath2[tpath2.index(self.region)-1] == other:
-                                bad.extend([target, source])
-                        except IndexError:
-                            pass
-
-                    if len(bad) > 4:
-                        raise ValueError, 'definition of bad is erroneous'
-
-                    if len(bad) == 2:
-                        #The reverse of the bad edge has a non-dependent tpath.
-                        #gp stands for good path.
-                        gp = copy.deepcopy(self.map_g[bad[1]][bad[0]]['tpath'])
-
-                        gp.reverse()
-
-                        #Set the dependent tpath to be the reverse of the good
-                        #one.
-                        self.map_g[bad[0]][bad[1]]['tpath'] = gp
-
-                    elif len(bad) == 4:
-                        #Both edges are dependent -- gotta remove 'em.
-                        self.map_g.remove_edge(source, target)
-                        self.map_g.remove_edge(target, source)
-                        break
-
-                    else:
+                    #We need to see whether the region in tpath1 after or
+                    #before self.region is other. But we need to handle the
+                    #possibility that self.region is the first or last
+                    #region in tpath.
+                    try:
+                        if tpath1[tpath1.index(self.region)+1] == other:
+                            bad.extend([source, target])
+                    except IndexError:
                         pass
+                    try:
+                        if tpath1[tpath1.index(self.region)-1] == other:
+                            bad.extend([source, target])
+                    except IndexError:
+                        pass
+                else:
+                    pass
+                
+                if (tpath2 == [self.region, other] or
+                    tpath2 == [other, self.region]):
+                    bad.extend([target, source])
+                elif self.region in set(tpath2):
+
+                    try:
+                        if tpath2[tpath2.index(self.region)+1] == other:
+                            bad.extend([target, source])
+                    except IndexError:
+                        pass
+                    try:
+                        if tpath2[tpath2.index(self.region)-1] == other:
+                            bad.extend([target, source])
+                    except IndexError:
+                        pass
+                else:
+                    pass
+
+                if len(bad) > 4:
+                    raise ValueError, 'definition of bad is erroneous'
+
+                if len(bad) == 2:
+                    #The reverse of the bad edge has a non-dependent tpath.
+                    #gp stands for good path.
+                    gp = copy.deepcopy(self.map_g[bad[1]][bad[0]]['tpath'])
+
+                    gp.reverse()
+
+                    #Set the dependent tpath to be the reverse of the good
+                    #one.
+                    self.map_g[bad[0]][bad[1]]['tpath'] = gp
+
+                elif len(bad) == 4:
+                    #Both edges are dependent -- gotta remove 'em.
+                    self.bad_edges.add((source, target))
+                    self.bad_edges.add((target, source))
 
                 else:
-                    finished = True
- 
+                    pass
+
     def handleS(self, others, rcs):
         """
         """
@@ -168,7 +171,7 @@ class Eliminator(object):
             #If this S is of category 2, keep it and remove the other edges.
             if fin_autom(d.get_word(tpath)) == 2:
                 others.pop(index)
-                self.remove_edges(others)
+                self.keep_track_of_edges_to_remove(others)
                 break
             start = rcs.index('S', start) + 1
             badSs.append(others[index])
@@ -176,7 +179,7 @@ class Eliminator(object):
         #If we didn't break, we didn't find an S of category 2. So we'll remove
         #the Ss.
         else:
-            self.remove_edges(badSs)
+            self.keep_track_of_edges_to_remove(badSs)
 
     def handleL(self, others, rcs):
         """
@@ -198,7 +201,7 @@ class Eliminator(object):
             #If this S is of category 2, remove the Ls and keep everything
             #else.
             if fin_autom(d.get_word(tpath)) == 2:
-                self.remove_edges(Ls)
+                self.keep_track_of_edges_to_remove(Ls)
                 break
             start = rcs.index('S', start) + 1
 
@@ -209,13 +212,13 @@ class Eliminator(object):
                 tpath = self.map_g[others[Lindex]][self.region]['tpath']
                 if fin_autom(d.get_word(tpath)) == 3:
                     others.pop(Lindex)
-                    self.remove_edges(others)
+                    self.keep_track_of_edges_to_remove(others)
                     break
 
             #If no Ls of category 3, remove all the Ls and keep everything
             #else.
             else:
-                self.remove_edges(Ls)
+                self.keep_track_of_edges_to_remove(Ls)
 
     def eliminate_contradiction(self, contradictory_paths):
         """
@@ -227,7 +230,7 @@ class Eliminator(object):
             #If there's an I, keep it and remove everything else.
             if 'I' in rcs:
                 others.pop(rcs.index('I'))
-                self.remove_edges(others)
+                self.keep_track_of_edges_to_remove(others)
 
             #There's no I. If self.blank == to, there must be at least one S.
             #Keep an S of category 2 if one exists and remove everything else;
@@ -260,6 +263,7 @@ class Eliminator(object):
 
             count += 1
             print('Eliminate: %d/%d' % (count, len(self.map_g)))
+        self.map_g.remove_edges_from(self.bad_edges)
 
 #-----------------------------------------------------------------------------
 # Test Functions
@@ -297,14 +301,14 @@ def test_remove_edges():
 
     e = Eliminator(fake_map_g)
     e.region = 'A-1'
-    e.remove_edges(['B-1', 'B-2'])
+    e.keep_track_of_edges_to_remove(['B-1', 'B-2'])
 
-    desired_g.remove_edges_from([('A-1', 'B-1'), ('B-1', 'A-1'),
-                                 ('A-1', 'B-2'), ('B-2', 'A-1'),
-                                 ('C-1', 'D-1'), ('D-1', 'C-1')])
     desired_g['A-1']['E-1']['tpath'] = ['A-1', 'E-1']
 
     nt.assert_equal(e.map_g.edge, desired_g.edge)
+    nt.assert_equal(e.bad_edges, set([('A-1', 'B-1'), ('B-1', 'A-1'),
+                                      ('A-1', 'B-2'), ('B-2', 'A-1'),
+                                      ('C-1', 'D-1'), ('D-1', 'C-1')]))
 
 def test_iterate_nodes():
     #See third block of text in Appendix J.
@@ -321,29 +325,6 @@ def test_iterate_nodes():
     desired_g = copy.deepcopy(fake_map_g)
 
     e.iterate_nodes()
-    
-    desired_g.remove_edges_from([('A-1', 'B-1'), ('A-1', 'B-2'),
-                                 ('B-1', 'A-1'), ('B-2', 'A-1')])
-    
-    nt.assert_equal(e.map_g.edge, desired_g.edge)
-
-def test_eliminate_contradiction():
-    #See third block of text in Appendix J.
-    fake_map_g = nx.DiGraph()
-    fake_map_g.add_edge('A-1', 'B-1', tpath=['A-1', 'B-1'], RC='L')
-    fake_map_g.add_edge('B-1', 'A-1', tpath=['B-1', 'A-1'], RC='S')
-    fake_map_g.add_edge('A-1', 'B-2', tpath=['A-1', 'B-2'], RC='O')
-    fake_map_g.add_edge('B-2', 'A-1', tpath=['B-2', 'A-1'], RC='O')
-    fake_map_g.add_edge('A-1', 'B-3', tpath=['A-1', 'B-3'], RC='I')
-    fake_map_g.add_edge('B-3', 'A-1', tpath=['B-3', 'A-1'], RC='I')
-
-    e = Eliminator(fake_map_g)
-    e.region = 'A-1'
-    e.blank = 'to'
-
-    desired_g = copy.deepcopy(fake_map_g)
-
-    e.eliminate_contradiction((['B-1', 'B-2', 'B-3'], ['L', 'O', 'I']))
     
     desired_g.remove_edges_from([('A-1', 'B-1'), ('A-1', 'B-2'),
                                  ('B-1', 'A-1'), ('B-2', 'A-1')])
@@ -371,11 +352,11 @@ def test_eliminate_contradiction():
 
     desired_g2 = copy.deepcopy(fake_map_g2)
 
-    e2.eliminate_contradiction((['B-1', 'B-2'], ['S', 'S']))
+    e2.iterate_nodes()
 
     desired_g2.remove_edges_from([('A-1', 'B-2'), ('B-2', 'A-1')])
 
-    nt.assert_equal(e2.map_g.edge, desired_g2.edge)
+    nt.assert_equal(e2.map_g.edge['A-1'], desired_g2.edge['A-1'])
 
     #Test that third conditional works.
     fake_map_g = nx.DiGraph()
@@ -396,7 +377,7 @@ def test_eliminate_contradiction():
 
     desired_g = copy.deepcopy(fake_map_g)
 
-    e.eliminate_contradiction((['A-1', 'A-2', 'A-3'], ['L', 'O', 'O']))
+    e.iterate_nodes()
 
     desired_g.remove_edges_from([('A-1', 'B-1'), ('B-1', 'A-1')])
 
