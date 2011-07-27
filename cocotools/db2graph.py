602952c2f8aeb30300225048eccf138c04b536ab
Daniel Bliss <dbliss@berkeley.edu>
2011-07-27 01:03:10 -0700

    Write best_rc and path_code in TrGraph.

--- cocotools/db2graph.py
+++ cocotools/db2graph.py
@@ -49,6 +49,52 @@ class TrGraph(nx.DiGraph):
                 bits[bit[0]] = []
         return bits[0] + [node] + bits[1]
 
+    def best_rc(self, p, s):
+        
+        # The lower the index of a letter in the PDC hierarchy, the
+        # higher its precision.
+        pdc_hierarchy = ('A', 'C', 'H', 'L', 'D', 'F', 'J', 'N', 'B', 'G', 'E',
+                         'K', 'I', 'O', 'M', 'P', 'Q', 'R')
+
+        # Each edge has a list of RCs and a list of PDCs.  The index
+        # of the most precise PDC is the same as that of the RC to
+        # which it refers.  Call that index best_i.  To return the
+        # most precise RC, this value must be found.
+
+        # Each PDC will be compared to the most precise one seen so
+        # far; this comparison will be accomplished by reference to
+        # the latter's index in the PDC hierarchy.  Call this index
+        # best_seen_rank, and start it at 18 (one beyond the last in
+        # the hierarchy).
+
+        best_seen_rank = 18
+        edge_attr = self[p][s]
+        pdcs = edge_attr['PDC']
+        rcs = edge_attr['RC']
+        for i, pdc in enumerate(pdcs):
+            # Some entries in the XML lack PDCs; these were given PDC=None.
+            if not pdc:
+                continue
+            current_rank = pdc_hierarchy.index(pdc)
+            if current_rank < best_seen_rank:
+                best_seen_rank = current_rank
+                best_i = i
+            elif current_rank == best_seen_rank and rcs[i] != rcs[best_i]:
+                raise ValueError('Conflicting RCs for (%s, %s)' % (p, s))
+        try:
+            return rcs[best_i]
+        except UnboundLocalError:
+            # If best_i hasn't been set, there are no PDCs for this edge
+            # and any RC is as good as any other.  Return the first one.
+            return edge_attr['RC'][0]
+
+    def path_code(self, p, tp, s):
+        best_rc = self.best_rc
+        middle = ''
+        for i in range(len(tp) - 1):
+            middle += best_rc(tp[i], tp[i + 1])
+        return best_rc(p, tp[0]) + middle + best_rc(tp[-1], s)
+
     def deduce_edges(self):
         """Deduce new edges based on those in the graph.
         
@@ -76,6 +122,7 @@ class TrGraph(nx.DiGraph):
                 for s in self.successors(node):
                     if p.split('-')[0] != s.split('-')[0]:
                         tp = self.tr_path(p, node, s)
+                        tpc = self.path_code(p, tp, s)
 
 
 class XMLReader(object):
--- cocotools/tests/test_db2graph.py
+++ cocotools/tests/test_db2graph.py
@@ -7,7 +7,7 @@ import xml.etree.ElementTree as etree
 from unittest import TestCase
 
 # Third party
-from mocker import MockerTestCase, MATCH
+from mocker import Mocker, MockerTestCase, IN
 import networkx as nx
 
 # Local
@@ -18,6 +18,32 @@ from cocotools import db2graph as d2g
 #------------------------------------------------------------------------------
 
 class TestTrGraph(TestCase):
+
+    def test_path_code(self):
+        mocker = Mocker()
+        g = mocker.mock()
+        g.best_rc(IN(['A', 'B']), IN(['B', 'C']))
+        mocker.result('I')
+        mocker.count(2)
+        g.best_rc('X', 'A')
+        mocker.result('S')
+        g.best_rc('C', 'Y')
+        mocker.result('L')
+        mocker.replay()
+        path_code = d2g.TrGraph.path_code.im_func
+        self.assertEqual(path_code(g, 'X', ['A', 'B', 'C'], 'Y'), 'SIIL')
+        mocker.restore()
+        mocker.verify()
+
+
+    def test_best_rc(self):
+        g = nx.DiGraph()
+        ebunch = (('A-1', 'B-1', {'RC': ['O', 'I'], 'PDC': ['A', 'A']}),
+                  ('B-1', 'C-1', {'RC': ['I', 'S'], 'PDC': ['A', 'H']}))
+        g.add_edges_from(ebunch)
+        best_rc = d2g.TrGraph.best_rc.im_func
+        self.assertRaises(ValueError, best_rc, g, 'A-1', 'B-1')
+        self.assertEqual(best_rc(g, 'B-1', 'C-1'), 'I')
 
     def test_tr_path(self):
         g = nx.DiGraph()
