f2301a062bfa3fe0b798f76f98d124034cf38cd4
Daniel Bliss <dbliss@berkeley.edu>
2011-07-29 16:42:49 -0700

    Make tons of progress with graphs.
    * Put allowed values for PDCs, RCs, and ECs in a constant dict.
    * Introduce new class ReGraph, which CoGraph and TrGraph subclass.
    * Start writing ReGraph method add_transformed_edges.
    * Write ReGraph methods valid_attr and clean_attr.
    * Move update method from CoGraph to ReGraph; expand it.
    * Start writing CoGraph method best_ecs.
    * Rename TrGraph method tr_path tp; refactor it.
    * Refactor TrGraph method best_rc.
    * Change TrGraph method rc_res from static method to regular one.
    * Fully update unit and system tests for finished methods.

--- cocotools/graphs.py
+++ cocotools/graphs.py
@@ -2,6 +2,8 @@
 # Imports
 #------------------------------------------------------------------------------
 
+from __future__ import print_function
+
 # Third party
 import networkx as nx
 
@@ -10,23 +12,115 @@ from cocotools.sqlite import LocalDB
 from cocotools.parse_xml import XMLReader
 
 #------------------------------------------------------------------------------
+# Constants
+#------------------------------------------------------------------------------
+
+# The lower the index of a letter in PDC, the higher its precision.
+ALLOWED_VALUES = {'PDC': ('A', 'C', 'H', 'L', 'D', 'F', 'J', 'N', 'B', 'G',
+                          'E', 'K', 'I', 'O', 'M', 'P', 'Q', 'R'),
+                  'RC': ('I', 'S', 'L', 'O', 'SO', 'LO', 'ISLO'),
+                  'EC': ('C', 'P', 'X', 'N')}
+
+#------------------------------------------------------------------------------
 # Classes
 #------------------------------------------------------------------------------
 
-class CoGraph(nx.DiGraph):
+class ReGraph(nx.DiGraph):
+
+    def add_transformed_edges(self, mapp, conn, desired_bmap):
+        count = 0
+        for source, target in conn.edges():
+            source_ec, target_ec = conn.best_ecs(source, target)
+            new_sources = transform(source, source_ec, desired_bmap)
+            new_targets = transform(target, target_ec, desired_bmap)
+            for new_source in new_sources:
+                for new_target in new_targets:
+                    new_attr = {'source_ec': new_sources[new_source],
+                                'target_ec': new_targets[new_target]}
+                    self.update(new_source, new_target, new_attr)
+            count += 1
+            print('AT: %d/%d' % (count, conn.number_of_edges()), end='\r')
+
+    def valid_attr(self, attr):
+        """Return bool corresponding to whether attr has valid ECs or RC."""
+        if 'RC' in attr:
+            for value in attr['RC']:
+                if value in ALLOWED_VALUES['RC']:
+                    return True
+        elif 'source_ec' in attr and 'target_ec' in attr:
+            for label in ('source_ec', 'target_ec'):
+                for value in attr[label]:
+                    if value in ALLOWED_VALUES['EC']:
+                        break
+                else:
+                    return False
+            return True
+        return False
+
+    def update(self, source, target, new_attr):
+        """Add new edge data to the graph.
+
+        First call self.clean_attr(new_attr) and then
+        self.valid_attr(new_attr); abort if latter returns False.
+        """
+        self.clean_attr(new_attr)
+        if not self.valid_attr(new_attr):
+            return
+        if not self.has_edge(source, target):
+            self.add_edge(source, target, new_attr)
+        else:
+            old_attr = self[source][target]
+            for key, new_value in new_attr.iteritems():
+                old_attr[key] += new_value
+
+    def clean_attr(self, attr):
+        """Clean invalid entries in attr.
+
+        Lowercase letters of the correct set are capitalized; invalid
+        characters are changed to None.
+
+        Returns
+        -------
+        attr : dict
+          Same as input but with invalid entries changed either to valid
+          ones or None.
+
+        Notes
+        -----
+        attr is modified in place.
+
+        U is a disallowed EC.
+        """
+        lowercase, invalid = [], []
+        for key, values in attr.iteritems():
+            try:
+                allowed_values = ALLOWED_VALUES[key.split('_')[-1].upper()]
+            except KeyError:
+                continue
+            for i, value in enumerate(values):
+                if value not in allowed_values:
+                    try:
+                        possible_correction = value.upper()
+                    except AttributeError:
+                        invalid.append((key, i))
+                    else:
+                        if possible_correction in allowed_values:
+                            lowercase.append((key, i))
+                        else:
+                            invalid.append((key, i))
+        for key, i in lowercase:
+            attr[key][i] = attr[key][i].upper()
+        for key, i in invalid:
+            attr[key][i] = None
+        return attr
+
+
+class CoGraph(ReGraph):
 
     def __init__(self):
         nx.DiGraph.__init__(self)
         self.table = 'Connectivity'
 
-    def update(self, source, target, edge_attr):
-        if not self.has_edge(source, target):
-            self.add_edge(source, target, edge_attr)
-        else:
-            edge_dict = self[source][target]
-            for key in edge_attr.iterkeys():
-                edge_dict[key].append(edge_attr[key][0])
-    
     def add_edges_from_bmaps(self, bmaps=False):
         table = self.table
         db = LocalDB()
@@ -39,7 +133,96 @@ class CoGraph(nx.DiGraph):
             for prim in reader.prim_iterator:
                 source, target, edge_attr = reader.prim2data(prim)
                 self.update(source, target, edge_attr)
-    
+
+    def best_ecs(self, source, target):
+        """Return the most precise ECs available for the edge.
+
+        Notes
+        -----
+        Precision matters only when a node has contradictory ECs.  In
+        this case, two PDCs are available to assist a choice: a PDC for
+        the EC and a PDC for the node.  The PDC for the EC is given
+        priority here, but if this produces a tie, processing proceeds
+        through the following steps until a difference is found.
+
+        1) If none of the tied ECs is N, choose X.
+
+        2) Choose the EC associated with the higher node-PDC.
+
+        3) Choose the majority EC among those tied, counting P and C as
+           X.
+
+        4) Choose the majority EC among all the ECs for the node, again
+           counting P and C as X.
+
+        If these steps are insufficient, a ValueError is raised so the
+        edge can be handled manually.
+        """
+        edge_attr = self[source][target]
+        best_ecs = []
+        for node in ('source', 'target'):
+            ecs = edge_attr['%s_ec' % node]
+            if len(set(ecs)) > 1:
+                ec_pdcs = edge_attr['%s_ec_pdc' % node]
+                for i, ec_pdc in enumerate(ec_pdcs):
+                    pass
+                
+        #     else:
+        #         best_ecs.append(ecs[0])
+            
+        #     best_seen_rank = 18
+        #     pdcs = edge_attr['%s_pdc' % node]
+        #         try:
+        #             current_rank = PDC_HIERARCHY.index(ec_pdc)
+        #         except ValueError:
+        #             continue
+        #         if current_rank < best_seen_rank:
+        #             best_seen_rank = current_rank
+        #             best_i = i
+        #         elif current_rank == best_seen_rank and ec_pdcs[i] != ec_pdcs[best_i]:
+        #             best_i = (best_i, i)
+        #     try:
+        #         if len(best_i) > 1:
+        #             msg = 'Conflicting ECs for %s of (%s, %s)'
+        #             raise ValueError(msg % (node, source, target))
+        #     except UnboundLocalError:
+        #         # If best_i hasn't been set, there are no PDCs for
+        #         # this node's EC; processing should move to the node's
+        #         # PDCs. 
+        #         for i, pdc in enumerate(pdcs):
+        #             try:
+        #                 current_rank = PDC_HIERARCHY.index(pdc)
+        #             except ValueError:
+        #                 continue
+        #             if current_rank < best_seen_rank:
+        #                 best_seen_rank = current_rank
+        #                 best_i = i
+        #             elif current_rank == best_seen_rank and pdcs[i] != pdcs[best_i]:
+        #                 best_i = (best_i, i)
+        #         try:
+        #             if len(best_i) > 1:
+        #                 msg = 'Conflicting ECs for %s of (%s, %s)'
+        #                 raise ValueError(msg % (node, source, target))
+        #         except UnboundLocalError:
+        #             # If best_i hasn't been set, there are no PDCs for this node
+        #             # and any EC is as good as any other.  Return the first one.
+        #             best_ecs.append(ecs[0])
+        #         except TypeError:
+        #             # If best_i has no len(), it's an int, indicating a single
+        #             # best.
+        #             best_ecs.append(ecs[best_i])
+        #             del current_rank, best_i
+        #         else:
+        #             del current_rank, best_i
+        #     except TypeError:
+        #         # If best_i has no len(), it's an int, indicating a single
+        #         # best.
+        #         best_ecs.append(ecs[best_i])
+        #         del current_rank, best_i
+        #     else:
+        #         del current_rank, best_i
+        # return best_ecs
+
 
 class TrGraph(CoGraph):
 
@@ -47,24 +230,26 @@ class TrGraph(CoGraph):
         nx.DiGraph.__init__(self)
         self.table = 'Mapping'
     
-    def tr_path(self, p, node, s):
+    def tp(self, p, node, s):
+        """Return the shortest path from p, through node, to s.
+
+        Returns
+        -------
+        list
+          Of nodes, not including p (start point) and s (end point).
+        """
         bits = {}
-        for bit in ((0, p, node), (1, node, s)):
-            tps = self[bit[1]][bit[2]]['TP']
+        for i, edge in enumerate([(p, node), (node, s)]):
+            tps = self[edge[0]][edge[1]]['TP']
             shortest = tps[0]
             for tp in tps:
                 if len(tp) < len(shortest):
                     shortest = tp
-            bits[bit[0]] = tp
+            bits[i] = shortest
         return bits[0] + [node] + bits[1]
 
     def best_rc(self, p, s):
         
-        # The lower the index of a letter in the PDC hierarchy, the
-        # higher its precision.
-        pdc_hierarchy = ('A', 'C', 'H', 'L', 'D', 'F', 'J', 'N', 'B', 'G', 'E',
-                         'K', 'I', 'O', 'M', 'P', 'Q', 'R')
-
         # Each edge has a list of RCs and a list of PDCs.  The index
         # of the most precise PDC is the same as that of the RC to
         # which it refers.  Call that index best_i.  To return the
@@ -81,10 +266,10 @@ class TrGraph(CoGraph):
         pdcs = edge_attr['PDC']
         rcs = edge_attr['RC']
         for i, pdc in enumerate(pdcs):
-            # Some entries in the XML lack PDCs; these were given PDC=None.
-            if not pdc:
+            try:
+                current_rank = ALLOWED_VALUES['PDC'].index(pdc)
+            except ValueError:
                 continue
-            current_rank = pdc_hierarchy.index(pdc)
             if current_rank < best_seen_rank:
                 best_seen_rank = current_rank
                 best_i = i
@@ -109,8 +294,7 @@ class TrGraph(CoGraph):
             middle += best_rc(tp[i], tp[i + 1])
         return best_rc(p, tp[0]) + middle + best_rc(tp[-1], s)
 
-    @staticmethod
-    def rc_res(tpc):
+    def rc_res(self, tpc):
         map_step = {'I': {'I': 'I', 'S': 'S', 'L': 'L', 'O': 'O'},
                     'S': {'I': 'S', 'S': 'S'},
                     'L': {'I': 'L', 'S': 'ISLO', 'L': 'L', 'O': 'LO'},
@@ -158,7 +342,7 @@ class TrGraph(CoGraph):
             for p in self.predecessors(node):
                 for s in self.successors(node):
                     if p.split('-')[0] != s.split('-')[0]:
-                        tp = self.tr_path(p, node, s)
+                        tp = self.tp(p, node, s)
                         tpc = self.path_code(p, tp, s)
                         rc_res = self.rc_res(tpc)
                         if rc_res:
--- cocotools/tests/test_graphs.py
+++ cocotools/tests/test_graphs.py
@@ -7,7 +7,8 @@ from unittest import TestCase
 
 # Third party
 import networkx as nx
-from mocker import Mocker, IN
+from mocker import Mocker, IN, ANY
+from testfixtures import Replacer
 
 # Local
 import cocotools.graphs as cg
@@ -16,24 +17,73 @@ import cocotools.graphs as cg
 # Test Classes
 #------------------------------------------------------------------------------
 
-class TestCoGraph(TestCase):
+class TestReGraph(TestCase):
+
+    def test_valid_attr(self):
+        g = cg.ReGraph()
+        self.assertTrue(g.valid_attr({'RC': ['I']}))
+        self.assertFalse(g.valid_attr({'source_ec': ['X']}))
+        self.assertTrue(g.valid_attr({'source_ec': ['X'], 'target_ec': ['P']}))
+        self.assertFalse(g.valid_attr({'source_ec': [1], 'target_ec': ['P']}))
 
     def test_update(self):
-        a = nx.DiGraph()
-        d = a.copy()
-        ebunch = {'TP': ['C'], 'RC': 'I'}
-        cg.CoGraph.update.im_func(a, 'A', 'B', ebunch)
-        d.add_edge('A', 'B', ebunch)
-        for node in ('A', 'B'):
-            self.assertEqual(a.edge[node], d.edge[node])
+        def mock_clean_attr(self, attr):
+            return attr
+        def mock_valid_attr(self, attr):
+            return True
+        with Replacer() as r:
+            r.replace('cocotools.graphs.ReGraph.clean_attr', mock_clean_attr)
+            r.replace('cocotools.graphs.ReGraph.valid_attr', mock_valid_attr)
+            g = cg.ReGraph()
+            edge_count = g.number_of_edges
+            self.assertEqual(edge_count(), 0)
+            g.update('A', 'B', {'TP': [['D']], 'RC': ['S']})
+            self.assertEqual(edge_count(), 1)
+            edge = g['A']['B']
+            self.assertEqual(edge, {'TP': [['D']], 'RC': ['S']})
+            g.update('A', 'B', {'TP': [['E']], 'RC': ['I']})
+            self.assertEqual(edge, {'TP': [['D'], ['E']], 'RC': ['S', 'I']})
+
+    def test_clean_attr(self):
+        a = {'RC': ['i'],
+             'PDC': ['A', '-', 'H', None],
+             'source_ec': ['U'],
+             'source_ec_pdc': ['-']}
+        d = {'RC': ['I'],
+             'PDC': ['A', None, 'H', None],
+             'source_ec': [None],
+             'source_ec_pdc': [None]}
+        self.assertEqual(cg.ReGraph.clean_attr.im_func(None, a), d)
+        # A new dict has not been created: a has been altered.
+        self.assertEqual(a, d)
+
+
+class TestCoGraph(TestCase):
+
+    def test_best_ecs(self):
+        pass
+    #     g = nx.DiGraph()
+    #     edge_attr = {'source_ec': ['X', 'P'],
+    #                  'source_pdc': ['A', 'L'],
+    #                  'source_ec_pdc': ['-', 'C'],
+    #                  'target_ec': ['C', 'N'],
+    #                  'target_pdc': ['J', 'H'],
+    #                  'target_ec_pdc': ['-', '-']}
+    #     ebunch = (('A-1', 'A-2', edge_attr), ('B-1', 'B-2', edge_attr))
+    #     g.add_edges_from(ebunch)
+    #     g['B-1']['B-2']['source_ec_pdc'] = ['C', 'C']
+    #     self.assertEqual(g['A-1']['A-2']['source_ec_pdc'][0], '-')
+    #     best_ecs = cg.CoGraph.best_ecs.im_func
+    #     self.assertEqual(best_ecs(g, 'A-1', 'A-2'), ['P', 'N'])
+    #     self.assertRaises(ValueError, best_ecs, g, 'B-1', 'B-2')
 
 
 class TestTrGraph(TestCase):
 
     def test_rc_res(self):
-        rc_res = cg.TrGraph.rc_res
-        self.assertEqual(rc_res('IIISSSIII'), 'S')
-        self.assertFalse(rc_res('LOSL'))
+        rc_res = cg.TrGraph.rc_res.im_func
+        self.assertEqual(rc_res(None, 'IIISSSIII'), 'S')
+        self.assertFalse(rc_res(None, 'LOSL'))
 
     def test_path_code(self):
         mocker = Mocker()
@@ -60,10 +110,10 @@ class TestTrGraph(TestCase):
         self.assertRaises(ValueError, best_rc, g, 'A-1', 'B-1')
         self.assertEqual(best_rc(g, 'B-1', 'C-1'), 'I')
 
-    def test_tr_path(self):
+    def test_tp(self):
         g = nx.DiGraph()
-        ebunch = (('A-1', 'B-1', {'TP': [[]]}),
-                  ('B-1', 'C-1', {'TP': [['D-1']]}))
+        ebunch = (('A-1', 'B-1', {'TP': [['G-1'], []]}),
+                  ('B-1', 'C-1', {'TP': [['D-1'], ['E-1', 'F-1']]}))
         g.add_edges_from(ebunch)
-        self.assertEqual(cg.TrGraph.tr_path.im_func(g, 'A-1', 'B-1', 'C-1'),
+        self.assertEqual(cg.TrGraph.tp.im_func(g, 'A-1', 'B-1', 'C-1'),
                          ['B-1', 'D-1'])
