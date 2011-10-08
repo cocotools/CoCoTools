59c413247581971bcf4e654f542000829a564915
Daniel Bliss <dbliss@berkeley.edu>
2011-10-08 16:23:57 -0700

    Update test_endgraph.

--- cocotools/tests/test_endgraph.py
+++ cocotools/tests/test_endgraph.py
@@ -1,25 +1,13 @@
 from networkx import DiGraph
 import nose.tools as nt
 
-from cocotools import EndGraph, MapGraph
+from cocotools import EndGraph, MapGraph, ConGraph
 
 #------------------------------------------------------------------------------
 # Integration Tests
 #------------------------------------------------------------------------------
 
-def test_add_edges_from():
-    g = EndGraph()
-    g.add_edges_from([('A-1', 'A-2', {'ebunches_for': [('B', 'C')]}),
-                      ('A-1', 'A-2', {'ebunches_for': [('B', 'B')]}),
-                      ('A-1', 'A-2', {'ebunches_against': [('C', 'C')]})])
-    nt.assert_equal(g.number_of_edges(), 1)
-    nt.assert_equal(g['A-1']['A-2'],
-                    {'ebunches_for': [('B', 'C'), ('B', 'B')],
-                     'ebunches_against': [('C', 'C')]})
-
-
-def test_add_translated_edges():
-    
+def test_dan():
     m = MapGraph()
     ebunch = [('A-1', 'B-1', {'TP': [], 'PDC': 0, 'RC': 'S'}),
               ('A-2', 'B-1', {'TP': [], 'PDC': 0, 'RC': 'S'}),
@@ -70,6 +58,127 @@ def test_add_translated_edges():
                                       'ebunches_against': [('D', 'D')]})
     # Make sure c hasn't been modified.
     nt.assert_equal(c.number_of_edges(), 10)
+
+
+def test_ort():
+    m = MapGraph()
+    ebunch = [('A-1', 'B-1', {'TP': [], 'PDC': 0, 'RC': 'S'}),
+              ('A-2', 'B-1', {'TP': [], 'PDC': 0, 'RC': 'S'}),
+              ('A-4', 'B-2', {'TP': [], 'PDC': 0, 'RC': 'O'}),
+              ('A-4', 'B-3', {'TP': [], 'PDC': 0, 'RC': 'O'}),
+              ('A-5', 'B-2', {'TP': [], 'PDC': 0, 'RC': 'O'}),
+              ('A-5', 'B-3', {'TP': [], 'PDC': 0, 'RC': 'O'})]
+    m.add_edges_from(ebunch)
+    c = ConGraph()
+    c.add_edges_from([('A-1', 'A-4', {'EC_Source': 'C',
+                                      'EC_Target': 'C',
+                                      'Degree': '1',
+                                      'PDC_Site_Source': 0,
+                                      'PDC_Site_Target': 0,
+                                      'PDC_EC_Source': 2,
+                                      'PDC_EC_Target': 0,
+                                      'PDC_Density': 4}),
+                      ('A-2', 'A-4', {'EC_Source': 'N',
+                                      'EC_Target': 'Nc',
+                                      'Degree': '0',
+                                      'PDC_Site_Source': 9,
+                                      'PDC_Site_Target': 1,
+                                      'PDC_EC_Source': 4,
+                                      'PDC_EC_Target': 1,
+                                      'PDC_Density': 4}),
+                      # Following connections include regions from
+                      # desired_bmap. 
+                      ('B-3', 'B-4', {'EC_Source': 'C',
+                                      'EC_Target': 'X',
+                                      'Degree': 'X',
+                                      'PDC_Site_Source': 5,
+                                      'PDC_Site_Target': 10,
+                                      'PDC_EC_Source': 10,
+                                      'PDC_EC_Target': 5,
+                                      'PDC_Density': 18}),
+                      ('B-5', 'A-4', {'EC_Source': 'N',
+                                      'EC_Target': 'Nx',
+                                      'Degree': '0',
+                                      'PDC_Site_Source': 0,
+                                      'PDC_Site_Target': 18,
+                                      'PDC_EC_Source': 18,
+                                      'PDC_EC_Target': 18,
+                                      'PDC_Density': 18}),
+                      # Following connection has no mapping info.
+                      ('F-1', 'F-2', {'EC_Source': 'P',
+                                      'EC_Target': 'X',
+                                      'Degree': '2',
+                                      'PDC_Site_Source': 5,
+                                      'PDC_Site_Target': 10,
+                                      'PDC_EC_Source': 10,
+                                      'PDC_EC_Target': 5,
+                                      'PDC_Density': 18})])
+    e = EndGraph()
+    e.add_translated_edges(m, c, 'B')
+    nt.assert_equal(e.number_of_edges(), 5)
+    nt.assert_equal(e['B-1']['B-2'], {'EC_Source': 'P',
+                                      'EC_Target': 'P',
+                                      'PDC_Source': 3.75,
+                                      'PDC_Target': 9.0,
+                                      'original_maps': ('A', 'A')})
+    nt.assert_equal(e['B-1']['B-3'], {'EC_Source': 'P',
+                                      'EC_Target': 'P',
+                                      'PDC_Source': 3.75,
+                                      'PDC_Target': 9.0,
+                                      'original_maps': ('A', 'A')})
+    nt.assert_equal(e['B-3']['B-4'], {'EC_Source': 'C',
+                                      'EC_Target': 'X',
+                                      'PDC_Source': 7.5,
+                                      'PDC_Target': 7.5,
+                                      'original_maps': ('B', 'B')})
+    nt.assert_equal(e['B-5']['B-2'], {'EC_Source': 'N',
+                                      'EC_Target': 'Up',
+                                      'PDC_Source': 9.0,
+                                      'PDC_Target': 18.0,
+                                      'original_maps': ('B', 'A')})
+    nt.assert_equal(e['B-5']['B-3'], {'EC_Source': 'N',
+                                      'EC_Target': 'Up',
+                                      'PDC_Source': 9.0,
+                                      'PDC_Target': 18.0,
+                                      'original_maps': ('B', 'A')})
+    # Make sure c hasn't been modified.
+    nt.assert_equal(c.number_of_edges(), 5)
+
+
+def test_add_edges_from_dan():
+    g = EndGraph()
+    g.add_edges_from([('A-1', 'A-2', {'ebunches_for': [('B', 'C')]}),
+                      ('A-1', 'A-2', {'ebunches_for': [('B', 'B')]}),
+                      ('A-1', 'A-2', {'ebunches_against': [('C', 'C')]})])
+    nt.assert_equal(g.number_of_edges(), 1)
+    nt.assert_equal(g['A-1']['A-2'],
+                    {'ebunches_for': [('B', 'C'), ('B', 'B')],
+                     'ebunches_against': [('C', 'C')]})
+
+
+def test_add_edges_from_ort():
+    g = EndGraph()
+    g.add_edges_from([('A-1', 'A-2', {'EC_Source': 'X',
+                                      'EC_Target': 'X',
+                                      'PDC_Source': 5,
+                                      'PDC_Target': 7,
+                                      'original_maps': ('B', 'B')}),
+                      ('A-1', 'A-2', {'EC_Source': 'X',
+                                      'EC_Target': 'X',
+                                      'PDC_Source': 3.44445,
+                                      'PDC_Target': 7,
+                                      'original_maps': ('C', 'C')}),
+                      ('A-1', 'A-2', {'EC_Source': 'Up',
+                                      'EC_Target': 'C',
+                                      'PDC_Source': 0,
+                                      'PDC_Target': 0,
+                                      'original_maps': ('D', 'D')})])
+    nt.assert_equal(g.number_of_edges(), 1)
+    nt.assert_equal(g['A-1']['A-2'], {'EC_Source': 'X',
+                                      'EC_Target': 'X',
+                                      'PDC_Source': 3.44445,
+                                      'PDC_Target': 7,
+                                      'original_maps': ('C', 'C')})
     
 #------------------------------------------------------------------------------
 # Unit Tests
