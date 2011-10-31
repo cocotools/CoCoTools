22472a08fb01fb50018cdd0ad545278cb60ca3db
Fernando Perez <Fernando.Perez@berkeley.edu>
2011-10-31 15:01:43 -0700

    Update graph drawing code for larger anatomical graphs.
    Fix color label in gold_standard file.

--- cocotools/nxdraw.py
+++ cocotools/nxdraw.py
@@ -1,6 +1,5 @@
 """Improved drawing of directed graphs in NetworkX.
 
-
 Modified from http://groups.google.com/group/networkx-discuss/browse_thread/thread/170624d22c4b0ee6?pli=1 
 
 Author: Stefan van der Walt.
@@ -10,42 +9,51 @@ import matplotlib.pyplot as plt
 from matplotlib.patches import FancyArrowPatch, Circle
 import numpy as np
 
-def draw_network(G,pos,sg=None,node_color=None,cmap=None):
-    f = plt.figure()
-    ax = f.gca()
+def draw_network(G, pos, node_color=None, ax=None,
+                 radius=1, fontsize=16, line_alpha=0.25,
+                 only_nodes = None):
+    if ax is None:
+        ax = f.gca()
+
+    if only_nodes is None:
+        only_nodes = set(G.nodes())
 
     for n in G:
-        if node_color is None or cmap is None:
-            color = (0,0,0)
+        if node_color is None:
+            color = (0, 0, 0)
         else:
-            color = cmap(node_color[n])
+            color = node_color[n]
+        #if node_color is None or cmap is None:
+        #    color = (0,0,0)
+        #else:
+        #    color = cmap(node_color[n])
 
-        c=Circle(pos[n],radius=0.13,alpha=0.5,color=color,ec='k')
+        c = Circle(pos[n],radius=radius,
+                   alpha=0.75,
+                   color=color,ec='k')
         x, y = pos[n]
         plt.text(x, y, n,
                 horizontalalignment='center',
                 verticalalignment='center',
                 color='k',
-                fontsize=20,
+                fontsize=fontsize,
                 )
-        plt.text(x, y, n,
-                 horizontalalignment='center',
-                 verticalalignment='center',
-                 color='w',
-                 fontsize=18,
-                 )
-        ax.add_patch(c)
+        c = ax.add_patch(c)
+        c.set_zorder(2)
         G.node[n]['patch']=c
         x,y=pos[n]
     seen={}
     for (u,v,d) in G.edges(data=True):
+        if not (u in only_nodes or v in only_nodes):
+            continue
+        
         n1=G.node[u]['patch']
         n2=G.node[v]['patch']
         rad=0.1
         if (u,v) in seen:
             rad=seen.get((u,v))
             rad=(rad + np.sign(rad)*0.1)*-1
-        alpha=0.5
+        alpha=line_alpha
         color = 'k'
         e = FancyArrowPatch(n1.center,n2.center,patchA=n1,patchB=n2,
                             arrowstyle='-|>',
@@ -55,13 +63,16 @@ def draw_network(G,pos,sg=None,node_color=None,cmap=None):
                             alpha=alpha,
                             color=color)
         seen[(u,v)]=rad
-        ax.add_patch(e)
+        e = ax.add_patch(e)
+        e.set_zorder(0.5)
 
-    ax.autoscale()
-    plt.axis('equal')
+    #ax.autoscale()
+    #plt.axis('equal')
+    plt.axis('tight')
     plt.axis('off')
 
-    return e
+    #return e
+    return ax
 
 if __name__ == "__main__":
     import networkx as nx
@@ -71,7 +82,7 @@ if __name__ == "__main__":
 
     pos=nx.spring_layout(G)
     ax=plt.gca()
-    draw_network(G,pos,ax)
+    draw_network(G,pos,ax=ax)
     ax.autoscale()
     plt.axis('equal')
     plt.axis('off')
