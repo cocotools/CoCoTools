32b49549edda5a2598aecf48813ddd0d1450b816
Daniel Bliss <dbliss@berkeley.edu>
2011-11-22 22:30:26 -0800

    Commit before making big changes to approach.

--- cocotools/ec_merge.py
+++ cocotools/ec_merge.py
@@ -1,3 +1,15 @@
+# An X or P with an N is as good as a U.  To conclude known-absence,
+# we need to know about the missing part, but we can never get it.
+#
+# Edges going into this module are specific observations.  Edges
+# coming out are guesses at reality.
+# 
+# All ECs w/out N reduce to Xx, because we don't know which region
+# was injected and whether more than necessary or too little was injected:
+#
+# Note: CoCoMac curators DO know which region was injected; the Access
+# DB has this info.
+
 # The lowercase part of the ECs that use this dict in the same pass
 # refer to the same other region (which has an RC to the other region
 # in the desired bmap).
@@ -9,6 +21,11 @@
 # Answering the question, How should we update our tentative
 # conclusion -- that ___ amount of A connects to ___ amount of B --
 # given that ___ amount of C connects to ___ amount of B?
+
+# Because one of our regions has been injected, we are always missing
+# some information: specifically, whether we needed to inject as much
+# of the region as we did to see the observed amount of dye in the
+# other region.  We must ignore this shortcoming.
 
 # Notes:
 # (1) For 'B' at least, lowercase EC doesn't matter.
@@ -27,103 +44,115 @@
 # equivalent.
 #
 # (8) RC=S should never result in a U.
+#
+# (9) Cp and Pc are better than Xx.  But C > X > P.  And when Xx is
+# the best representation of what we know, use that.
+#
+# (10) Anything partnered with an N that's not C implies the
+# unconsidered part has an unknown amount of connection.  For example,
+# if part of a region connects, and the other part doesn't, we say Pc,
+# not Pn.
+#
+#
+# The following reductions can be made:
+#
+# Present: Any combinations of C, P, and X.
+# Absent: C with N.
+# Unknown: N with P or X.
 
-so_ec_merge = {'B': {'S': {'Nc': 'Nc',
-                           'Nx': 'Nx',
-                           'Np': 'Np',
-                           'Cn': 'Cn',
-                           'Cc': 'Cc',
-                           'Cx': 'Cx',
-                           'Cp': 'Cp',
-                           'Xn': 'Xn',
-                           'Xc': 'Xc',
-                           'Xx': 'Xx',
-                           'Xp': 'Xp',
-                           'Pn': 'Pn',
-                           'Pc': 'Pc',
-                           'Px': 'Px',
-                           'Pp': 'Pp',
+
+# If ever you have an S to a region with a Present EC, you're gonna
+# stay present.  The only way to be absent is to have RCs only to
+# things that are absent.  An RC of O leads to Unknown unless EC is Absent.
+
+
+so_ec_merge = {'B': {'S': {'Present': 'Present',
+                           'Absent': 'Absent',
+                           'Unknown': 'Unknown'
                            }
-                     'O': {'Nc': 'Nc',
-                           'Nx': 'Nx',
-                           'Np': 'Np',
-                           'Cn': 'Cn',
-                           'Cc': 'Cc',
-                           'Cx': 'Cx',
-                           'Cp': 'Cp',
-                           'Xn': 'U',
-                           'Xc': 'U',
-                           'Xx': 'U',
-                           'Xp': 'U',
-                           'Pn': 'U',
-                           'Pc': 'U',
-                           'Px': 'U',
-                           'Pp': 'U',
+                     'O': {'Present': 'Unknown',
+                           'Absent': 'Absent',
+                           'Unknown': 'Unknown'
                            }
                      }
-               'Nc': {'S': {'Nc': 'Nc',
-                            'Nx': 'Nx',
-                            'Np': 'Np',
-                            'Cn': 'Nc',
-                            'Cc': 'Pc',
-                            'Cx': 'Px',
-                            'Cp': 'Pp',
-                            'Xn': 'Pn',
-                            'Xc': 'Pc',
-                            'Xx': 'Px',
-                            'Xp': 'Pp',
-                            'Pn': 'Pn',
-                            'Pc': 'Pc',
-                            'Px': 'Px',
-                            'Pp': 'Pp',
-                            }
+               'Present': {'S': {'Present': 'Present',
+                                 'Absent': 'Present',
+                                 'Unknown': 'Present'
+                                 }
                       'O': {'Nc': 'Nc',
                             'Nx': 'Nx',
                             'Np': 'Np',
                             'Cn': 'Nc',
-                            'Cc': 'Pc',
-                            'Cx': 'Px',
-                            'Cp': 'Pp',
                             'Xn': 'U',
-                            'Xc': 'U',
                             'Xx': 'U',
-                            'Xp': 'U',
-                            'Pn': 'U',
-                            'Pc': 'U',
-                            'Px': 'U',
-                            'Pp': 'U',
+                            'Pn': 'U'
                             }
                       }
                'Nx': {'S': {'Nc': 'Nx',
                             'Nx': 'U', # Could be different part injected
                             'Np': 'U', # Could be different part injected
                             'Cn': 'Nx',
-                            'Cc': 'Pc',
-                            'Cx': 'Px',
-                            'Cp': 'Pp',
-                            'Xn': 'Pn', # This much is true.
-                            'Xc': 'Pc',
+                            'Xn': 'Xn', # This much is true.
                             'Xx': 'Px',
-                            'Xp': 'Pp',
                             'Pn': 'Pn', # This much is true.
-                            'Pc': 'Pc',
-                            'Px': 'Px',
-                            'Pp': 'Pp',
+                            }
+                      'O': {'Nc': 'Nx',
+                            'Nx': 'U', 
+                            'Np': 'U',
+                            'Cn': 'Nx',
+                            'Xn': 'U',
+                            'Xx': 'U',
+                            'Pn': 'U',
+                            }
+                      }
+               'Np': {'S': {'Nc': 'Np',
+                            'Nx': 'U',
+                            'Np': 'U',
+                            'Cn': 'Np',
+                            'Xn': 'Pn',
+                            'Xx': 'Px',
+                            'Pn': 'Pn',
+                            }
+                      'O': {'Nc': 'Np',
+                            'Nx': 'U', 
+                            'Np': 'U',
+                            'Cn': 'Np',
+                            'Xn': 'U',
+                            'Xx': 'U',
+                            'Pn': 'U'
+                            }
+                      }
+               'Cn': {'S': {'Nc': 'Cn',
+                            'Nx': 'Nx',
+                            'Np': 'Np',
+                            'Cn': 'Cn',
+                            'Xn': 'Xn',
+                            'Xx': 'Px',
+                            'Pn': 'Pn'
+                            }
+                      'O': {'Nc': 'Cn',
+                            'Nx': 'Nx', 
+                            'Np': 'Np',
+                            'Cn': 'Cn',
+                            'Xn': 'U',
+                            'Xx': 'U',
+                            'Pn': 'U'
+                            }
+                      }
+               'Xn': {'S': {'Nc': 'Xn',
+                            'Nx': 'Xn',
+                            'Np': 'Xn',
+                            'Cn': 'Xn',
+                            'Xn': 'Xn',
+                            'Xx': 'Px',
+                            'Pn': 'Xn'
                             }
                       'O': {'Nc': '',
-                            'Nx': '',
+                            'Nx': '', 
                             'Np': '',
                             'Cn': '',
-                            'Cc': '',
-                            'Cx': '',
-                            'Cp': '',
                             'Xn': '',
-                            'Xc': '',
                             'Xx': '',
-                            'Xp': '',
-                            'Pn': '',
-                            'Pc': '',
-                            'Px': '',
-                            'Pp': '',
+                            'Pn': ''
                             }
                       }
