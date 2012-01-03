0e011aab1ae669427d728593e28d205753a0e1d4
Daniel Bliss <dbliss@berkeley.edu>
2012-01-02 20:04:49 -0800

    Improve ORT script; add internal O RCs to brain_maps.

--- cocotools/brain_maps.py
+++ cocotools/brain_maps.py
@@ -95,6 +95,35 @@ CONN_FAILURES = ['AB89', 'ABMR98', 'ABP80', 'AF42', 'AF45', 'B09', 'B81',
 # ('DU86-MST', 'DU86-DMZ'), ('DU86-MTP', 'DU86-DMZ'),
 # ('UD86A-DMZ', 'UD86A-MTP'), ('UD86A-MTP', 'UD86A-DMZ')
 INTRAMAP_O_RCS = ['DU86', 'UD86a', 'BF95', 'SP89b', 'RAP87', 'SP89a', 'SMKB95']
+# Implicit ones:
+# [('FV91-VOT', 'FV91-V4'), mediator = GSG88-V4
+#  ('PHT00-32', 'PHT00-9/32'), mediator = PP94-32
+#  ('PHT00-32', 'PHT00-8/32'), mediator = PP94-32
+#  ('PHT00-2/1', 'PHT00-1'), mediator = B09-1
+#  ('PHT00-2/1', 'PHT00-2'), mediator = B09-2
+#  ('PHT00-8/32', 'PHT00-32'), mediator = PP94-32
+#  ('PHT00-31', 'PHT00-PGM/31'), mediator = VPR87-31
+#  ('PHT00-PGM/31', 'PHT00-31'), mediator = VPR87-31
+#  ('PHT00-PGM/31', 'PHT00-PGM'), mediator = PS82-PGM
+#  ('PHT00-9/32', 'PHT00-32'), mediator = PP94-32
+#  ('SA90-36/TF/TE', 'SA90-TE'), mediator = IAC87A-TE
+#  ('LV00A-TPT', 'LV00A-TOC'), mediator = PG91B-TPT
+#  ('L34-PROS.B', 'L34-SUB.'), mediator = RV87-SUB
+#  ('PHT00-23A', 'PHT00-24/23A'), mediator = VPR87-23A
+#  ('PHT00-23C', 'PHT00-24/23C'), mediator = VPR87-23C
+#  ('PHT00-23B', 'PHT00-24/23B'), mediator = VPR87-23B
+#  ('BB47-TEO', 'BB47-OA'), mediator = B09-19
+#  ('PHT00-1', 'PHT00-2/1'), mediator = B09-1
+#  ('PHT00-2', 'PHT00-2/1'), mediator = B09-2
+#  ('FV91-V4', 'FV91-VOT'), mediator = GSG88-V4
+#  ('L34-SUB.', 'L34-PROS.B'), mediator = RV87-SUB
+#  ('BB47-OA', 'BB47-TEO'), mediator = B09-19
+#  ('LV00A-TOC', 'LV00A-TPT'), mediator = PG91B-TPT
+#  ('PHT00-24/23A', 'PHT00-23A'), mediator = VPR87-23A
+#  ('PHT00-24/23C', 'PHT00-23C'), mediator = VPR87-23C
+#  ('PHT00-24/23B', 'PHT00-23B'), mediator = VPR87-23B
+#  ('PHT00-PGM', 'PHT00-PGM/31'), mediator = PS82-PGM
+#  ('SA90-TE', 'SA90-36/TF/TE')] mediator = IAC87A-TE
 
 
 # The spatial hierarchy within these maps cannot be determined based
--- scripts/ort.py
+++ scripts/ort.py
@@ -2,39 +2,64 @@ import pickle
 
 import cocotools as coco
 
-# Get conn.
-con_bunch, con_failures = coco.multi_map_ebunch('Connectivity',
-                                                coco.CONN_SUCCESSES)
-assert(len(con_failures) == 0)
-conn = coco.ConGraph()
-conn.add_edges_from(con_bunch)
+#------------------------------------------------------------------------------
+# conn1.pck
+#------------------------------------------------------------------------------
 
-# Save conn1.
-with open('results/post_sfn_revisions/conn1.pck', 'w') as f:
-    pickle.dump(conn, f)
+response = raw_input('Build new ConGraph or use old one (n/o)? ')
+if response == 'n':
+    con_bunch, con_failures = coco.multi_map_ebunch('Connectivity',
+                                                    coco.CONN_SUCCESSES)
+    assert(len(con_failures) == 0)
+    conn = coco.ConGraph()
+    conn.add_edges_from(con_bunch)
+    with open('results/post_sfn_revisions/conn1.pck', 'w') as f:
+        pickle.dump(conn, f)
+elif response == 'o':
+    with open('results/post_sfn_revisions/conn1.pck') as f:
+        conn = pickle.load(f)
 
-# Get mapp.
-map_bunch, map_failures = coco.multi_map_ebunch('Mapping', coco.MAPP_SUCCESSES)
-assert(len(map_failures) == 0)
-mapp = coco.MapGraph(conn)
-map_bunch += coco.MISSING_MAPP_EDGES
-mapp.add_edges_from(map_bunch)
+#------------------------------------------------------------------------------
+# mapp1.pck
+#------------------------------------------------------------------------------
 
-# Save mapp1.
-with open('results/post_sfn_revisions/mapp1.pck', 'w') as f:
-    pickle.dump(mapp, f)
+response = raw_input('Build new MapGraph or use old one (n/o)? ')
+if response == 'n':
+    map_bunch, map_failures = coco.multi_map_ebunch('Mapping',
+                                                    coco.MAPP_SUCCESSES)
+    assert(len(map_failures) == 0)
+    mapp = coco.MapGraph(conn)
+    map_bunch += coco.MISSING_MAPP_EDGES
+    mapp.add_edges_from(map_bunch)
+    with open('results/post_sfn_revisions/mapp1.pck', 'w') as f:
+        pickle.dump(mapp, f)
+elif response == 'o':
+    with open('results/post_sfn_revisions/mapp1.pck') as f:
+        mapp = pickle.load(f)
 
-# Deduce new mapp edges.
-mapp.deduce_edges()
+#------------------------------------------------------------------------------
+# mapp2.pck
+#------------------------------------------------------------------------------
 
-# Save mapp2.
-with open('results/post_sfn_revisions/mapp2.pck', 'w') as f:
-    pickle.dump(mapp, f)
+response = raw_input('Has deduce_edges already been called (y/n)? ')
+if response == 'n':
+    mapp.deduce_edges()
+    with open('results/post_sfn_revisions/mapp2.pck', 'w') as f:
+        pickle.dump(mapp, f)
+elif response == 'y':
+    with open('results/post_sfn_revisions/mapp2.pck') as f:
+        mapp = pickle.load(f)
 
-# Create endg.
-endg = coco.EndGraph()
-endg.add_translated_edges(mapp, mapp.conn, 'PHT00')
+#------------------------------------------------------------------------------
+# endg.pck
+#------------------------------------------------------------------------------
 
-# Save endg.
-with open('results/post_sfn_revisions/endg.pck', 'w') as f:
-    pickle.dump(endg, f)
+response = raw_input('Make new EndGraph (y/n)? ')
+if response == 'y':
+    endg = coco.EndGraph()
+    endg.add_translated_edges(mapp, mapp.conn, 'PHT00')
+    with open('results/post_sfn_revisions/endg.pck', 'w') as f:
+        pickle.dump(endg, f)
+elif response == 'n':
+    with open('results/post_sfn_revisions/endg.pck') as f:
+        endg = pickle.load(f)
