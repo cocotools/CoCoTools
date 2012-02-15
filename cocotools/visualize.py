12b2b58b5070588adae430d55de71c10efbecde7
Daniel Bliss <dbliss@berkeley.edu>
2012-02-14 16:48:28 -0800

    Raise error if there are nodes in the graph not in the atlas.

--- cocotools/visualize.py
+++ cocotools/visualize.py
@@ -10,20 +10,21 @@ from matplotlib import pyplot as plt
 from cocotools import nxdraw
 
 
+class VisualizeError(Exception):
+    pass
+
+
 def load_labels(fname='cocotools/coords/standard_labels.csv',
                 usecols=None):
-    """Load the standard labels files returning a record array with the atlas.
+    """Load the standard labels file; return a record array with the atlas.
     """
-
     # Converters for the fields we're interested in: strings without whitespace
     # and floats
     cleanstr = lambda x: str(x).strip()
-
     conv = {0:cleanstr, 1:cleanstr, 2:cleanstr, 3:cleanstr, 4:float, 5:float,
             6:float, 7:cleanstr, 8:cleanstr, 9:int}
-
+    # Exclude comments column by default.
     usecols = range(7)+[8] if usecols is None else usecols
-    
     with open(fname) as f:
         # Read the first line for the names
         all_names = [n.strip() for n in f.next().split(',')]
@@ -39,7 +40,6 @@ def load_labels(fname='cocotools/coords/standard_labels.csv',
             # Now, let genfromtxt iterate over the rest of the file
             araw = np.genfromtxt(fdata, delimiter='|', usecols=usecols,
                                  converters=conv)
-
     # Rename the fields so the dtype has more useful names than f0, f1, etc.
     # Also make it a recarray for more convenient use further down
     names = [all_names[i] for i in usecols]
@@ -87,34 +87,21 @@ def make_projection(atlas):
     return labels, positions, aspect, colors
 
 
-def load_graphs(fname, atlas):
-    """Load a graph and return both the original and the common-nodes one.
-
-    This reads a pickled graph and returns a pair: the graph just loaded and
-    one that contains only nodes common with those in the atlas.
+def load_graph(fname, atlas):
+    """Load a graph, update its node labels, and return it.
     """
-    g_coco = pickle.load(open(fname))
-
-    # relabel nodes to remove 'PHT00-' prefix
-    remove_pht_map = dict([(lab, lab.replace('PHT00-','')) for lab in g_coco])
-    g_coco = nx.relabel_nodes(g_coco, remove_pht_map)
-
-    # relabel graph according to cocomac nodes
+    g = pickle.load(open(fname))
+    # Relabel graph according to cocomac nodes.  Nodes whose labels
+    # are not keys in name_map are left alone.
     name_map = dict(zip(atlas.cocomac, atlas.label))
-    g = nx.relabel_nodes(g_coco, name_map)
-    common = set(g.nodes()).intersection(set(atlas.label))
-
-    gnorm = nx.DiGraph()
-    for node in common:
-        gnorm.add_node(node)
-    for u,v,data in g.edges(data=True):
-        if u in common and v in common:
-                gnorm.add_edge(u, v, data)
-
-    return g, gnorm
+    g = nx.relabel_nodes(g, name_map)
+    nodes_not_in_atlas = set(g.nodes()).difference(set(atlas.label))
+    if nodes_not_in_atlas:
+        raise VisualizeError('%s not in atlas' % nodes_not_in_atlas)
+    return g
 
 
-def graph_view(projection, graph, aspect, colors, positions, size=None,
+def graph_view(projection, graph, aspect, colors, positions, size='small',
                only_nodes=None, node_alpha=None, edge_alpha=None, title=None):
     """Simple utility to draw the various projections.
 
@@ -124,19 +111,15 @@ def graph_view(projection, graph, aspect, colors, positions, size=None,
       'axial', 'sagittal', or 'coronal'.
     """
     # Large size, suitable for high-quality PDFs
-    sizes = dict(axial=8, sagittal=15, coronal=8)
+    large = dict(axial=8, sagittal=15, coronal=8)
     # Smaller defaults for interactive work
-    sizes = dict(axial=5, sagittal=10, coronal=5)
-    
-    if size is None:
-        size = sizes[projection]
-        
+    small = dict(axial=5, sagittal=10, coronal=5)
+    sizes = {'large': large, 'small': small}
+    size = sizes[size][projection]
     f, ax = plt.subplots(figsize=(size, size/aspect[projection]))
     if title is None:
         title = "%s Projection" % projection.title()
-        
     ax.set_title(title, fontsize=20)
-    
     # Smart defaults for node/edge transpanrency: for full graphs, it's better
     # to make the edges faint and the nodes darker
     if only_nodes is None: n_alpha=0.7; e_alpha=0.1
@@ -144,14 +127,13 @@ def graph_view(projection, graph, aspect, colors, positions, size=None,
     # User can still override by giving values
     node_alpha = n_alpha if node_alpha is None else node_alpha
     edge_alpha = e_alpha if edge_alpha is None else edge_alpha
-        
     nxdraw.draw_network(graph, pos=positions[projection], ax=ax,
                         node_color=colors, only_nodes=only_nodes,
                         node_alpha=node_alpha, edge_alpha=edge_alpha)
-    
     if projection=='axial':
         ax.set_xlim(-3,30)
     return ax
+
 
 def view_PHT00(path_to_graph, projection, only_nodes=None):
     """Display graph.
@@ -165,7 +147,6 @@ def view_PHT00(path_to_graph, projection, only_nodes=None):
     """
     atlas = load_labels()
     labels, positions, aspect, colors = make_projection(atlas)
-    g, gnorm = load_graphs(path_to_graph, atlas)
-    graph_view(projection, gnorm, aspect, colors, positions,
-               only_nodes=only_nodes)
+    g = load_graph(path_to_graph, atlas)
+    graph_view(projection, g, aspect, colors, positions, only_nodes=only_nodes)
     plt.show()
