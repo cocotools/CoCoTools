80e752e80aaf789e2675ce192eeeadefdc8371b3
Daniel Bliss <dbliss@berkeley.edu>
2012-06-26 16:48:11 -0700

    Add new module for querying each area of TO maps separately.

---
+++ cocotools/query_by_area.py
@@ -0,0 +1,384 @@
+import copy
+import sqlite3
+import os
+import errno
+import re
+import urllib
+import urllib2
+import xml.etree.ElementTree as etree
+from cStringIO import StringIO
+from socket import timeout
+
+from brain_maps import MAPPING_TIMEOUTS, CONNECTIVITY_TIMEOUTS, TIMEOUT_AREAS
+
+
+PDC_HIER = ('A', 'C', 'H', 'L', 'D', 'F', 'J', 'N', 'B', 'G', 'E', 'K', 'I',
+            'O', 'M', 'P', 'Q', 'R', None)
+DBPATH = os.path.join(os.environ['HOME'], '.cache', 'cocotools.sqlite')
+DBDIR = os.path.dirname(DBPATH)
+P = './/{http://www.cocomac.org}'
+SPECS = {'Mapping': {'data_set': 'PrimRel', 'primtag': 'PrimaryRelation',
+                     'other_tags': ('RC', 'PDC')},
+         'Connectivity': {'data_set': 'IntPrimProj',
+                          'primtag': 'IntegratedPrimaryProjection',
+                          'other_tags': ('PDC_Site', 'EC', 'PDC_EC', 'Degree',
+                                         'PDC_Density')}}
+
+
+class _CoCoLite(object):
+
+    def __init__(self, func):
+        self.func = func
+        self.con = self.setup_connection()
+
+    def setup_connection(self):
+        try:
+            os.mkdir(DBDIR)
+        except OSError, e:
+            if e.errno != errno.EEXIST:
+                raise
+        con = sqlite3.connect(DBPATH)
+        con.text_factory = str
+        with con:
+            con.execute("""
+CREATE TABLE IF NOT EXISTS cache
+(
+    bmap TEXT,
+    type TEXT,
+    xml TEXT UNIQUE
+)
+""")
+        return con
+
+    def __call__(self, search_type, bmap):
+        try:
+            xml = self.select_xml(search_type, bmap)
+        except IndexError:
+            xml = self.func(search_type, bmap)
+            if xml:
+                with self.con as con:
+                    con.execute("""
+INSERT INTO cache
+VALUES (?, ?, ?)
+""", (bmap, search_type, xml))
+        return xml
+
+    def select_xml(self, search_type, bmap):
+        rows = self.con.execute("""
+SELECT xml
+FROM cache
+WHERE bmap = ? AND type = ?
+""", (bmap, search_type)).fetchall()
+        if len(rows) > 1:
+            raise sqlite3.IntegrityError('multiple xml entries for bmap %s' %
+                                         bmap)
+        return rows[0][0]
+
+    def remove_entry(self, search_type, bmap):
+        self.con.execute("""
+DELETE FROM cache
+WHERE bmap = ? AND type = ?
+""", (bmap, search_type))
+        self.con.commit()
+
+#------------------------------------------------------------------------------
+# Private Functions
+#------------------------------------------------------------------------------
+
+def _scrub_element(e, attr_tag):
+    datum_e = e.find('%s%s' % (P, attr_tag))
+    try:
+        datum = datum_e.text
+    except AttributeError:
+        datum = None
+    else:
+        datum = datum.upper()
+        if datum == '-':
+            datum = None
+    if 'PDC' in attr_tag:
+        return PDC_HIER.index(datum)
+    if attr_tag == 'RC':
+        # Trial and error has shown that a region has an E to its
+        # laminae, and they have a C to it, though this seems like the
+        # opposite of the way it should be.
+        if datum == 'E':
+            return 'L'
+        elif datum == 'C':
+            return 'S'
+    return datum
+
+
+def _element2edge(prim_e, search_type):
+    """Extract and return source, target, and edge attributes from e.
+    
+    Call _scrub_element on each Element corresponding to an edge
+    attribute.
+
+    Parameters
+    ----------
+    prim : etree.Element
+      PrimaryRelation or IntegratedPrimaryProjection Element.
+
+    Returns
+    -------
+    source : string
+    target : string
+    edge_attr : dict
+
+    Notes
+    -----
+    Make BrainSites uppercase to avoid duplicate nodes (differing only
+    in case) being added to graph.
+    """
+    edge_attr = {}
+    for attr_tag in SPECS[search_type]['other_tags']:
+        if 'EC' in attr_tag or 'Site' in attr_tag:
+            specifiers = ('Source', 'Target')
+        else:
+            specifiers = ('',)
+        for specifier in specifiers:
+            if specifier:
+                site_e = prim_e.find('%s%sSite' % (P, specifier))
+                datum = _scrub_element(site_e, attr_tag)
+                key = '_'.join((attr_tag, specifier))
+                edge_attr[key] = datum
+            else:
+                datum = _scrub_element(prim_e, attr_tag)
+                edge_attr[attr_tag] = datum
+    if search_type == 'Connectivity':
+        edge_attr = _reduce_ecs(edge_attr)
+        # The next five lines put the ECs in a form needed by the
+        # classic version of ORT.
+        ec_s, ec_t = edge_attr['EC_Source'], edge_attr['EC_Target']
+        if ec_s == 'N':
+            ec_t = 'N%s' % ec_t.lower()
+        if ec_t == 'N':
+            ec_s = 'N%s' % ec_s.lower()
+    site_ids = prim_e.findall('%sID_BrainSite' % P)
+    return site_ids[0].text.upper(), site_ids[1].text.upper(), edge_attr
+
+
+def _reduce_ecs(attr):
+    """Determine the information conveyed by the ECs.
+
+    Unless the two ECs are C and N, specifying a genuinely absent
+    connection, the node that was injected may have been over-injected
+    or under-injected.  In an over-injection, parts of the region were
+    injected that do not connect to the other region; all the dye in the
+    other region is there because of just a small part of the full extent
+    of injection.  In an under-injection, more dye would have been seen in
+    the non-injected region had more of the injected region been injected.
+
+    To exacerbate the uncertainty this reasoning exposes, we don't even
+    know which region was injected (unless one EC is N).  When both ECs are
+    in (C, P, X) the best we can say is that some or all of the source
+    projects to some or all of the target -- both ECs are X or,
+    equivalently, the connection is present.  When one EC is P or X and the
+    other is N, we can't say anything about the connection because we don't
+    know whether the uninjected parts of the P/X region connect to the
+    other one.
+    """
+    s_ec, t_ec = attr['EC_Source'], attr['EC_Target']
+    present = ('C', 'P', 'X')
+    if s_ec in present and t_ec in present:
+        attr['Connection'] = 'Present'
+    elif s_ec + t_ec in ('NC', 'CN'):
+        attr['Connection'] = 'Absent'
+    else:
+        attr['Connection'] = 'Unknown'
+    return attr
+
+
+def _element_tree(xml_str):
+    s = StringIO()
+    s.write(xml_str)
+    s.seek(0)
+    return etree.parse(s)
+
+
+def _scrub_xml_str(raw):
+    """Remove spurious data before start of XML headers.
+
+    The CoCoMac server is returning the SQL query string prepended to the
+    output before the start of the XML header.  This routine strips all data
+    before the start of a valid XML header.
+
+    If the otuput does not contain any valid XML header, ValueError is raised.
+
+    The routine also removes a few invalid characters we've seen appear in
+    CoCoMac output.
+
+    Parameters
+    ----------
+    raw : string
+      Raw data returned by CoCoMac.
+
+    Returns
+    -------
+    xml : string
+      Cleaned-up XML.
+
+    Note
+    ----
+    Other than enforce that a valid XML header is present, this routine does
+    not validate the output as real XML.
+    """
+    match = re.match('(.*)(<\?xml.*\?>.*)', raw, re.S)
+    if match:
+        out =  match.group(2)
+        # The invalid characters seen do not cover exhaustively a
+        # specific range, so we've added them one-by-one to this
+        # string.
+        invalids = """\
+[\xb4\xfc\xd6\r\xdc\xe4\xdf\xf6\x85\xf3\xf2\x92\x96\xed\x84\x94\xb0]"""
+        return re.sub(invalids, '', out)
+    else:
+        raise ValueError('input does not contain valid xml header')
+
+#------------------------------------------------------------------------------
+# Public Functions
+#------------------------------------------------------------------------------
+
+def url(search_type, bmap):
+    """Return CoCoMac URL corresponding to XML query results.
+
+    Parameters
+    ----------
+    search_type : string
+      'Mapping' or 'Connectivity'
+
+    bmap : string
+      Name of a BrainMap in CoCoMac.
+
+    Returns
+    -------
+    string
+      URL corresponding to query results.
+    """
+    search_string = "('%s')[SourceMap]OR('%s')[TargetMap]" % (bmap, bmap)
+    query_dict = dict(user='teamcoco',
+                      password='teamcoco',
+                      Search=search_type,
+                      SearchString=search_string,
+                      DataSet=SPECS[search_type]['data_set'],
+                      OutputType='XML_Browser')
+    # The site appears to have changed from cocomac.org to 134.95.56.239:
+    return 'http://134.95.56.239/URLSearch.asp?' + urllib.urlencode(query_dict)
+
+
+@_CoCoLite
+def query_cocomac(search_type, bmap):
+    """Return XML corresponding to a CoCoMac query.
+
+    XML is returned as a string.  The local SQLite database is queried
+    first; only if the data file is not present there is the CoCoMac
+    website consulted.
+
+    Parameters
+    ----------
+    search_type : string
+      'Mapping' or 'Connectivity'
+
+    bmap : string
+      Name of a BrainMap in CoCoMac.
+
+    Returns
+    -------
+    string
+      XML containing query results.
+    """
+    try:
+        xml = urllib2.urlopen(url(search_type, bmap), timeout=120).read()
+    except (urllib2.URLError, timeout):
+        return
+    return _scrub_xml_str(xml)
+
+
+def single_area_ebunch(search_type, bmap):
+    """Construct and return ebunch from data for one BrainMap.
+
+    Parameters
+    ----------
+    search_type : string
+      'Mapping' or 'Connectivity'
+
+    bmap : string
+      Name of a BrainMap in CoCoMac.
+
+    Returns
+    -------
+    ebunch : list of tuples
+
+    Notes
+    -----
+    Integrated primary projections are returned for Connectivity queries,
+    and primary relations are returned for Mapping queries.
+    """
+    xml = query_cocomac(search_type, bmap)
+    if xml:
+        tree = _element_tree(xml)
+        ebunch = []
+        for prim in tree.iterfind('%s%s' % (P, SPECS[search_type]['primtag'])):
+            ebunch.append(_element2edge(prim, search_type))
+        if not ebunch:
+            query_cocomac.remove_entry(search_type, bmap)
+        if search_type == 'Mapping':
+            return ebunch
+        else:
+            return ebunch
+
+
+def query_maps_by_area(search_type, subset=False):
+    """Construct and return ebunch from data for several BrainMaps.
+
+    Also return the BrainMaps for which queries failed.
+
+    Parameters
+    ----------
+    search_type : string
+      'Mapping' or 'Connectivity'
+
+    subset : sequence or string (optional)
+      Subset of BrainMaps to query.  Default is to query all BrainMaps in
+      CoCoMac.  If a string is supplied, it must be the name of a text
+      file with one BrainMap per line.  
+
+    Returns
+    -------
+    big_ebunch : list of tuples
+      Each tuple specifies a source and target node and edge attributes.
+    
+    failures : list of strings
+      These are the BrainMaps for which no data were acquired.
+
+    Notes
+    -----
+    Querying a BrainMap for which there is only Mapping data using the
+    Connectivity search type would result in its being a failure.
+    Failures can also result from CoCoMac server errors.
+
+    The term ebunch, borrowed from NetworkX, refers to a sequence of
+    graph theory edges.
+    
+    Integrated primary projections are returned for Connectivity queries,
+    and primary relations are returned for Mapping queries.
+    """
+    if not subset:
+        if search_type == 'Mapping':
+            bmaps = MAPPING_TIMEOUTS
+        elif search_type == 'Connectivity':
+            bmaps = CONNECTIVITY_TIMEOUTS
+    elif isinstance(subset, str):
+        bmaps = [line.strip() for line in open(subset).readlines()]
+    else:
+        bmaps = subset
+    big_ebunch = []
+    failures = []
+    for bmap in bmaps:
+        for area in TIMEOUT_AREAS[bmap]:
+            little_ebunch = single_area_ebunch(search_type, bmap)
+            if little_ebunch:
+                big_ebunch += little_ebunch
+            else:
+                failures.append(bmap)
+    return big_ebunch, failures
---
+++ cocotools/tests/test_query_by_area.py
@@ -0,0 +1,228 @@
+import sqlite3
+import xml.etree.ElementTree as etree
+from testfixtures import replace, Replacer
+from unittest import TestCase
+
+import nose.tools as nt
+
+import cocotools.query_by_area as cq
+
+
+#------------------------------------------------------------------------------
+# Mock Functions
+#------------------------------------------------------------------------------
+
+def mock__scrub_element(e, attr_tag):
+    return 'X'
+
+
+def mock_url(search_type, bmap):
+    return 'http://www.google.com'
+
+
+def mock__scrub_xml_str(xml):
+    return xml[:10]
+
+
+def mock_func(search_type, bmap):
+    if search_type and bmap:
+        return 'xml %s %s xml' % (search_type, bmap)
+    return
+    
+    
+def mock_query_cocomac(search_type, bmap):
+    assert search_type == 'Mapping'
+    assert bmap == 'A'
+    return 'X'
+
+
+def mock__element_tree(xml):
+    tree = etree.parse(open('cocotools/tests/sample_map.xml'))
+    assert isinstance(tree, etree.ElementTree)
+    return tree
+
+
+def mock__element2edge(prim_e, search_type):
+    assert search_type == 'Mapping'
+    return ('node', 'node', 'edge_attr')
+
+
+def mock_single_map_ebunch(search_type, bmap):
+    if bmap in ('A', 'C', 'PP99'):
+        return [('node', 'node', 'edge_attr'), ('node', 'node', 'edge_attr')]
+
+#------------------------------------------------------------------------------
+# Private Function Unit Tests
+#------------------------------------------------------------------------------
+
+def test__reduce_ecs():
+    attr = {'EC_Source': 'X', 'EC_Target': 'N'}
+    nt.assert_equal(cq._reduce_ecs(attr), {'Connection': 'Unknown',
+                                           'EC_Source': 'X',
+                                           'EC_Target': 'N'})
+    attr = {'EC_Source': 'X', 'EC_Target': 'P'}
+    nt.assert_equal(cq._reduce_ecs(attr), {'Connection': 'Present',
+                                           'EC_Source': 'X',
+                                           'EC_Target': 'P'})
+    attr = {'EC_Source': 'N', 'EC_Target': 'C'}
+    nt.assert_equal(cq._reduce_ecs(attr), {'Connection': 'Absent',
+                                           'EC_Source': 'N',
+                                           'EC_Target': 'C'})
+    
+    
+class ScrubElementTestCase(TestCase):
+
+    def setUp(self):
+        with open('cocotools/tests/sample_con.xml') as xml:
+            tree = etree.parse(xml)
+        self.prim_e = tree.find('%sIntegratedPrimaryProjection' % cq.P)
+        self.assertTrue(etree.iselement(self.prim_e))
+
+    def test_rc_is_C(self):
+        with open('cocotools/tests/map_with_C.xml') as xml:
+            tree = etree.parse(xml)
+        prim_e = tree.find('%sPrimaryRelation' % cq.P)
+        self.assertEqual(cq._scrub_element(prim_e, 'RC'), 'S')
+
+    def test_from_prim(self):
+        self.assertEqual(cq._scrub_element(self.prim_e, 'Degree'), '0')
+
+    def test_from_site(self):
+        site_e = self.prim_e.find('%sSourceSite' % cq.P)
+        self.assertTrue(etree.iselement(site_e))
+        self.assertEqual(cq._scrub_element(site_e, 'EC'), 'N')
+
+    def test_missing_text(self):
+        self.assertEqual(cq._scrub_element(self.prim_e, 'XX'), None)
+
+    def test_bad_pdc_value(self):
+        self.assertEqual(cq._scrub_element(self.prim_e, 'PDC_EC'),
+                         18)
+
+
+@replace('cocotools.query_by_area._scrub_element', mock__scrub_element)
+def test__element2edge():
+    element2edge = cq._element2edge
+    # Mapping
+    with open('cocotools/tests/sample_map.xml') as xml:
+        prim_e = etree.parse(xml).find('%sPrimaryRelation' % cq.P)
+    nt.assert_true(etree.iselement(prim_e))
+    edge = ('B05-19', 'PP99-19', {'RC': 'X', 'PDC': 'X'})
+    nt.assert_equal(element2edge(prim_e, 'Mapping'), edge)
+    # Connectivity
+    with open('cocotools/tests/sample_con.xml') as xml:
+        prim_e = etree.parse(xml).find('%sIntegratedPrimaryProjection' % cq.P)
+    nt.assert_true(etree.iselement(prim_e))
+    edge = ('B05-2', 'PP99-9/46D', {'EC_Target': 'X',
+                                    'Degree': 'X',
+                                    'PDC_Site_Target': 'X',
+                                    'EC_Source': 'X',
+                                    'PDC_Density': 'X',
+                                    'PDC_EC_Target': 'X',
+                                    'PDC_Site_Source': 'X',
+                                    'PDC_EC_Source': 'X',
+                                    'Connection': 'Present'})
+    nt.assert_equal(element2edge(prim_e, 'Connectivity'), edge)
+
+
+def test_element_tree():
+    xml = open('cocotools/tests/sample_map.xml').read()
+    nt.assert_true(isinstance(cq._element_tree(xml), etree.ElementTree))
+    
+
+def test_scrub_xml_str():
+    scrub_xml_str = cq._scrub_xml_str
+    # Header
+    header = "SELECT  Top 32767  "
+    xmls  = ['<?xml version="1.0" encoding="UTF-8"?>',
+             '<?xml version="1.0" encoding="UTF-8"?>\n<Header>']
+    for xml in xmls:
+        nt.assert_equal(scrub_xml_str(header + xml), xml)
+        nt.assert_equal(scrub_xml_str(xml), xml)
+    nt.assert_raises(ValueError, scrub_xml_str, header)
+    # Body
+    texts = ['<?xml?>With \xb4junk',
+             '<?xml?>With <TextPageNumber>\xfc.200</TextPageNumber>']
+    valids = ['<?xml?>With junk',
+              '<?xml?>With <TextPageNumber>.200</TextPageNumber>']
+    for text, valid in zip(texts, valids):
+        nt.assert_equal(scrub_xml_str(text), valid)
+
+#------------------------------------------------------------------------------
+# _CoCoLite and query_cocomac Tests
+#------------------------------------------------------------------------------
+
+@replace('cocotools.query_by_area.url', mock_url)
+@replace('cocotools.query_by_area._scrub_xml_str', mock__scrub_xml_str)
+def test_query_cocomac():
+    # query_cocomac has been decorated.  Test __init__ and
+    # setup_connection.
+    nt.assert_true(isinstance(cq.query_cocomac.con, sqlite3.Connection))
+    # But the undecorated function can be used within the decorated one.
+    undecorated = cq.query_cocomac.func
+    nt.assert_equal(undecorated(None, None), '<!doctype ')
+    
+    
+@replace('cocotools.query_by_area.DBPATH', ':memory:')
+def test__CoCoLite():
+    db = cq._CoCoLite(mock_func)
+    # Test that mock_func works as expected.
+    nt.assert_equal(db.func(None, None), None)
+    nt.assert_equal(db.func('Mapping', 'A'), 'xml Mapping A xml')
+    # Test selection when cache has no matching entries (select_xml).
+    nt.assert_raises(IndexError, db.select_xml, 'Mapping', 'A')
+    # Test insertion (__call__, select_xml, and func).
+    nt.assert_equal(db('Mapping', 'A'), 'xml Mapping A xml')
+    # Test selection when cache has one matching entry (select_xml).
+    nt.assert_equal(db.select_xml('Mapping', 'A'), 'xml Mapping A xml')
+    # Test selection with __call__, mocking select_xml.
+    with Replacer() as r:
+        mock_select_xml = lambda self, s, b: 'stuff'
+        r.replace('cocotools.query_by_area._CoCoLite.select_xml', mock_select_xml)
+        nt.assert_equal(db('Mapping', 'A'), 'stuff')
+    # Test selection when cache has multiple matching entries
+    # (select_xml).
+    db.con.execute("""
+INSERT INTO cache
+VALUES ('A', 'Mapping', 'entry #2')
+""")
+    nt.assert_raises(sqlite3.IntegrityError, db.select_xml, 'Mapping', 'A')
+
+    
+@replace('cocotools.query_by_area.DBPATH', ':memory:')
+def test_remove_entry():
+    cq.query_cocomac.con.execute("""
+INSERT INTO cache
+VALUES ('A', 'B', 'Blah')
+""")
+    nt.assert_equal(cq.query_cocomac.select_xml('B', 'A'), 'Blah')
+    cq.query_cocomac.remove_entry('B', 'A')
+    nt.assert_raises(IndexError, cq.query_cocomac.select_xml, 'B', 'A')
+    
+#------------------------------------------------------------------------------
+# Public Function Unit Tests
+#------------------------------------------------------------------------------
+
+def test_url():
+    url = """http://134.95.56.239/URLSearch.asp?Search=Mapping&SearchString=\
+%28%27PP99%27%29%5BSourceMap%5DOR%28%27PP99%27%29%5BTargetMap%5D&user=\
+teamcoco&password=teamcoco&OutputType=XML_Browser&DataSet=PrimRel"""
+    nt.assert_equal(cq.url('Mapping', 'PP99'), url)
+
+    
+@replace('cocotools.query_by_area.query_cocomac', mock_query_cocomac)
+@replace('cocotools.query_by_area._element_tree', mock__element_tree)
+@replace('cocotools.query_by_area._element2edge', mock__element2edge)
+def test_single_map_ebunch():
+    ebunch = [('node', 'node', 'edge_attr') for i in range(4)]
+    nt.assert_equal(cq.single_map_ebunch('Mapping', 'A'), ebunch)
+
+    
+@replace('cocotools.query_by_area.single_map_ebunch', mock_single_map_ebunch)
+def test_multi_map_ebunch():
+    e, f = cq.multi_map_ebunch(None, ['A', 'B', 'C', 'D'])
+    nt.assert_equal(e, [('node', 'node', 'edge_attr') for i in range(4)])
+    nt.assert_equal(f, ['B', 'D'])
+    e, f = cq.multi_map_ebunch(None, 'cocotools/tests/sample_bmaps.txt')
+    nt.assert_equal(e, [('node', 'node', 'edge_attr') for i in range(2)])
+    nt.assert_equal(f, ['PP02'])
