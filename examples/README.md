1fa3d40b74037011e9f0e9ad4e063fdb13c51970
Fernando Perez <Fernando.Perez@berkeley.edu>
2013-02-20 14:54:51 -0800

    Add script to auto-generate example index and the index itself.

---
+++ examples/README.md
@@ -0,0 +1,8 @@
+# A collection of Notebooks for using CoCoTools
+
+The following notebooks showcase how to use CoCoTools interactively for
+querying the CoCoMac connectivity database.
+
+* [CoCoTools_tutorial_entire_corpus](http://nbviewer.ipython.org/url/github.com/cocotools/CoCoToolsraw/master/examples/CoCoTools_tutorial_entire_corpus.ipynb)
+* [CoCoTools_tutorial_small_dataset](http://nbviewer.ipython.org/url/github.com/cocotools/CoCoToolsraw/master/examples/CoCoTools_tutorial_small_dataset.ipynb)
+* [Table1](http://nbviewer.ipython.org/url/github.com/cocotools/CoCoToolsraw/master/examples/Table1.ipynb)
---
+++ scripts/mknbindex.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+"""Simple script to auto-generate the index of notebooks in a given directory.
+"""
+
+import glob
+import urllib
+
+notebooks = sorted(glob.glob('*.ipynb'))
+
+tpl = ( '* [{0}](http://nbviewer.ipython.org/url/github.com/cocotools/CoCoTools'
+        'raw/master/examples/{1})' )
+
+idx = [ 
+"""# A collection of Notebooks for using CoCoTools
+
+The following notebooks showcase how to use CoCoTools interactively for
+querying the CoCoMac connectivity database.
+"""]
+
+idx.extend(tpl.format(nb.replace('.ipynb',''), urllib.quote(nb)) 
+           for nb in notebooks)
+
+with open('README.md', 'w') as f:
+    f.write('\n'.join(idx))
+    f.write('\n')
