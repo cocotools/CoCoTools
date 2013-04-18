ef163081a316dbfd68d9f9b3256cf9ef1104bd15
Rob Blumenfeld <rsblume@gmail.com>
2013-04-18 13:32:03 -0700

    add_tarnslated_edge now endg method, cleaned nbs

--- cocotools/endgraph.py
+++ cocotools/endgraph.py
@@ -293,7 +293,7 @@ latter are not disjoint""" % (new, originals))
                 'PDC': np.mean(pdcs)}
 
     def _at_logic(self, ecs, rcs):
-        """Here I am, Rob! ;)"""
+        """Look up tables"""
         if len(rcs) == 1 and rcs[0] in ('I', 'L'):
             table = {'B': {'I': {'N': 'N',
                                  'P': 'P',
@@ -562,7 +562,8 @@ latter are not disjoint""" % (new, originals))
             translation_dict[new_node] = self._translate_node(mapp, new_node,
                                                               original_map)
         return translation_dict
-
+	
+	
     def add_translated_edges(self, mapp, conn, desired_map, method):
         """Translate edges in conn to nomenclature of desired_bmap.
 
@@ -608,3 +609,50 @@ latter are not disjoint""" % (new, originals))
                     new_source = s_mapping[0].split('-', 1)[-1]
                     new_target = t_mapping[0].split('-', 1)[-1]
                     self.add_edge(new_source, new_target, attr)
+
+    def add_translated_edge(self, mapp, conn, desired_map, method, edge):
+        """This function translates one edge in conn to nomenclature of desired_bmap.
+
+        Useful for testing registrations of specific edges or interrogating differences between ORT and mORT.
+
+        Parameters
+        ----------
+        mapp : MapGraph
+          Graph of spatial relationships between BrainSites from various
+          BrainMaps.
+
+        conn : ConGraph
+          Graph of anatomical connections between BrainSites.
+
+        desired_map : string
+          Name of BrainMap to which translation will be performed.
+
+        method : string
+          AT method to be used: 'original' (that of Stephan & Kotter)
+          or 'modified'
+        """
+        self.map = desired_map
+        # Add all target-map nodes to the EndGraph.  We need to search both
+        # the map and con graphs because one can contain nodes the other
+        # doesn't have.
+        for node in set(mapp.nodes()+conn.nodes()):
+            if node.split('-')[0] == desired_map:
+                self.add_node(node.split('-', 1)[-1])
+        at_setting = {'original': self._translate_attr_original,
+                      'modified': self._translate_attr_modified}
+        self._translate_attr = at_setting[method]
+        for original_s, original_t in edge:
+            s_dict = self._make_translation_dict(mapp, original_s, desired_map)
+            t_dict = self._make_translation_dict(mapp, original_t, desired_map)
+            for s_mapping in s_dict.iteritems():
+                for t_mapping in t_dict.iteritems():
+                    attr = self._translate_attr(s_mapping, t_mapping, mapp,
+                                                conn)
+                    # The first element in each mapping is the node in
+                    # desired_map.  We must remove the brain map name,
+                    # pre-pended to the name of the area.  (Note that
+                    # the brain map is stored as self.map.)
+                    new_source = s_mapping[0].split('-', 1)[-1]
+                    new_target = t_mapping[0].split('-', 1)[-1]
+                    self.add_edge(new_source, new_target, attr)
+    
